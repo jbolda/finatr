4a5bf610b3b209464260aa3caa97a42f1af578f5
Jake Robers <jakerobers@gmail.com>
2018-10-13 13:31:29 -0500

    test(transactionSemiannuallyReoccur): value

--- src/resolveFinancials/resolveTransaction.test.js
+++ src/resolveFinancials/resolveTransaction.test.js
@@ -397,4 +397,11 @@ describe('transactionSemiannuallyReoccur', () => {
     const next = transactionSemiannuallyReoccur({ transaction: transaction, seedDate: seedDate });
     expect(next.date).toEqual(startOfDay('2018-07-01'));
   });
+
+  it('has a value', () => {
+    const transaction = { value: 10 };
+    const seedDate = startOfDay('2018-01-01');
+    const next = transactionSemiannuallyReoccur({ transaction: transaction, seedDate: seedDate });
+    expect(next.y).toEqual(10);
+  });
 });
