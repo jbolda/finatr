8c637d854e3c731f44e9efee9aa5977fff29e319
Jake Robers <jakerobers@gmail.com>
2018-10-13 10:58:14 -0500

    refactor TabView into file with tests

---
+++ src/tabView.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TabView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: null };
+    if (props.tabTitles.length > 0) {
+      this.state.activeTab = 0;
+    }
+  }
+
+  tabClick(index) {
+    this.setState({ activeTab: index });
+  }
+
+  computeUrl(tabName) {
+    let url = tabName.toLowerCase();
+    url = url.replace(" ", "_");
+    return url;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="tabs">
+          <ul>
+            {this.props.tabTitles.map((tab, index) => (
+              <li
+                key={tab}
+                className={index === this.state.activeTab ? 'is-active' : ''}
+                onClick={this.tabClick.bind(this, index)}
+              >
+                <a href={`#/${this.computeUrl(tab)}`}>{tab}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="container is-fluid">
+          {
+            this.state.activeTab && this.props.tabContents[this.state.activeTab] ?
+            this.props.tabContents[this.state.activeTab]
+            : null
+          }
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+TabView.defaultProps = {
+  tabTitles: [],
+  tabContents: null,
+}
+
+TabView.propTypes = {
+  tabTitles: PropTypes.array,
+  tabContents: PropTypes.node,
+}
+
+export default TabView;
---
+++ src/tabView.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import TabView from './tabView';
+import renderer from 'react-test-renderer';
+
+describe(`computeUrl`, () => {
+  it(`replaces spaces with underscores`, () => {
+    let component = renderer.create(<TabView />)
+    const expected = component.getInstance().computeUrl('some url');
+    expect(expected).toEqual('some_url');
+  });
+
+  it(`lowercases`, () => {
+    let component = renderer.create(<TabView />)
+    const expected = component.getInstance().computeUrl('URL');
+    expect(expected).toEqual('url');
+  });
+});
+
+describe(`tabClick`, () => {
+  it(`sets the activeTab`, () => {
+    let component = renderer.create(<TabView />).getInstance();
+    component.tabClick(0);
+    expect(component.state.activeTab).toEqual(0);
+  });
+});
+
+describe(`initial state`, () => {
+  it(`has activeTab=null when there aren't any tabTitles`, () => {
+    let component = renderer.create(<TabView />).getInstance();
+    expect(component.state.activeTab).toEqual(null);
+  });
+
+  it('has activeTab=0 when there are tabTitles', () => {
+    let component = renderer.create(<TabView tabTitles={['some title']}/>).getInstance();
+    expect(component.state.activeTab).toEqual(0);
+  })
+});
+
