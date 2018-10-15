34339ee582c17c089b7572a4898110a146ef2c54
Jake Robers <jakerobers@gmail.com>
2018-10-15 06:46:15 -0500

    add(transactionAnnuallyReoccur): tests (#34)

--- src/resolveFinancials/resolveTransactions.js
+++ src/resolveFinancials/resolveTransactions.js
@@ -204,8 +204,24 @@ const transactionDayOfMonthReoccur = ({
 
 // when transaction.rtype === 'bimonthly'
 const transactionBimonthlyReoccur = ({ transaction, seedDate }) => {
+  if (!transaction) {
+    throw new Error("transactionSemiannuallyReoccur expects { transaction }")
+  }
+
+  if (!transaction.value) {
+    transaction.value = 0;
+  }
+
+  if (!transaction.cycle) {
+    transaction.cycle = 1;
+  }
+
+  if (!seedDate) {
+    throw new Error("transactionSemiannuallyReoccur expects { seedDate }")
+  }
+
   return {
-    date: setDate(transaction.cycle)(addMonths(2)(seedDate)),
+    date: addMonths(2*transaction.cycle)(seedDate),
     y: transaction.value,
     dailyRate: transaction.value / 30 / 2
   };
@@ -238,12 +254,16 @@ const transactionQuarterlyReoccur = ({ transaction, seedDate }) => {
 
 // when transaction.rtype === 'semiannually'
 const transactionSemiannuallyReoccur = ({ transaction, seedDate }) => {
-  if (!transaction || !transaction.value) {
+  if (!transaction) {
     throw new Error("transactionSemiannuallyReoccur expects { transaction }")
   }
 
   if (!seedDate) {
     throw new Error("transactionSemiannuallyReoccur expects { seedDate }")
+  }
+
+  if (!transaction.value) {
+    transaction.value = 0;
   }
 
   return {
@@ -255,12 +275,16 @@ const transactionSemiannuallyReoccur = ({ transaction, seedDate }) => {
 
 // when transaction.rtype === 'annually'
 const transactionAnnuallyReoccur = ({ transaction, seedDate }) => {
-  if (!transaction || !transaction.value) {
+  if (!transaction) {
     throw new Error("transactionAnnuallyReoccur expects { transaction }")
   }
 
   if (!seedDate) {
     throw new Error("transactionAnnuallyReoccur expects { seedDate }")
+  }
+
+  if (!transaction.value) {
+    transaction.value = 0;
   }
 
   return {
