013c3836ee5274fa324a933ce4ef5552275bd91d
Jacob Bolda <me@jacobbolda.com>
2018-11-22 20:57:13 -0600

    hook up YNAB form (#40)
    hook up YNAB form

--- src/forms/ynabInput.js
+++ src/forms/ynabInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Formik, Field } from 'formik';
+import { Consumer } from '@microstates/react';
 import * as ynab from 'ynab';
 import getDay from 'date-fns/fp/getDay';
 import getDate from 'date-fns/fp/getDate';
@@ -13,61 +14,72 @@ class YNABInput extends React.Component {
         <h2 className="subtitle has-text-centered">
           This all happens in your browser and is only exchanged with YNAB.
         </h2>
-        <Formik
-          initialValues={{
-            devToken: this.props.initialDevToken
-              ? this.props.initialDevToken
-              : ``,
-            budgetId: this.props.initialBudgetId
-              ? this.props.initialBudgetId
-              : ``,
-            importTransactions: false
-          }}
-          enableReinitialize={true}
-          onSubmit={(values, actions) => {
-            let ynabAPI = new ynab.API(values.devToken);
-            ynabAPI.accounts
-              .getAccounts(values.budgetId)
-              .then(accountResponse => {
-                console.log('accounts', accountResponse);
-                let ynabAccounts = accountResponse.data.accounts
-                  .filter(account => account.closed === false)
-                  .map(account => {
-                    return {
-                      name: account.name,
-                      starting: Math.abs(account.cleared_balance) / 1000,
-                      interest: 0,
-                      vehicle: 'operating'
-                    };
-                  });
-                let ynabScheduledTransactions = [];
-                if (values.importTransactions) {
-                  ynabAPI.scheduledTransactions
-                    .getScheduledTransactions(values.budgetId)
-                    .then(scheduledTransactionResponse => {
-                      console.log('transactions', scheduledTransactionResponse);
-                      scheduledTransactionResponse.data.scheduled_transactions.forEach(
-                        transaction => {
-                          let ynabSanitizedFrequency = consumeYNABRepeat(
-                            transaction,
+        <Consumer>
+          {model => (
+            <Formik
+              initialValues={{
+                devToken: model.forms.ynabForm.devToken.state,
+                budgetId: model.forms.ynabForm.budgetId.state,
+                importTransactions: false
+              }}
+              enableReinitialize={true}
+              onSubmit={(values, actions) => {
+                let ynabAPI = new ynab.API(values.devToken);
+                ynabAPI.accounts
+                  .getAccounts(values.budgetId)
+                  .then(accountResponse => {
+                    console.log('accounts', accountResponse);
+                    let ynabAccounts = accountResponse.data.accounts
+                      .filter(account => account.closed === false)
+                      .map(account => {
+                        return {
+                          name: account.name,
+                          starting: Math.abs(account.cleared_balance) / 1000,
+                          interest: 0,
+                          vehicle: 'operating'
+                        };
+                      });
+                    let ynabScheduledTransactions = [];
+                    if (values.importTransactions) {
+                      ynabAPI.scheduledTransactions
+                        .getScheduledTransactions(values.budgetId)
+                        .then(scheduledTransactionResponse => {
+                          console.log(
+                            'transactions',
+                            scheduledTransactionResponse
+                          );
+                          scheduledTransactionResponse.data.scheduled_transactions.forEach(
+                            transaction => {
+                              let ynabSanitizedFrequency = consumeYNABRepeat(
+                                transaction,
+                                ynabScheduledTransactions
+                              );
+                              ynabScheduledTransactions.push({
+                                id: transaction.id,
+                                raccount: transaction.account_name,
+                                category:
+                                  consumeYNABType(transaction) === 'transfer'
+                                    ? 'transfer'
+                                    : transaction.category_name,
+                                type: consumeYNABType(transaction),
+                                start: transaction.date_first,
+                                rtype: ynabSanitizedFrequency.repeat,
+                                cycle: ynabSanitizedFrequency.cycle,
+                                value: Math.abs(transaction.amount) / 1000
+                              });
+                            }
+                          );
+                          model.addYNAB(
+                            {
+                              devToken: values.devToken,
+                              budgetId: values.budgetId
+                            },
+                            ynabAccounts,
                             ynabScheduledTransactions
                           );
-                          ynabScheduledTransactions.push({
-                            id: transaction.id,
-                            raccount: transaction.account_name,
-                            category:
-                              consumeYNABType(transaction) === 'transfer'
-                                ? 'transfer'
-                                : transaction.category_name,
-                            type: consumeYNABType(transaction),
-                            start: transaction.date_first,
-                            rtype: ynabSanitizedFrequency.repeat,
-                            cycle: ynabSanitizedFrequency.cycle,
-                            value: Math.abs(transaction.amount) / 1000
-                          });
-                        }
-                      );
-                      this.props.addYNAB(
+                        });
+                    } else {
+                      model.addYNAB(
                         {
                           devToken: values.devToken,
                           budgetId: values.budgetId
@@ -75,85 +87,91 @@ class YNABInput extends React.Component {
                         ynabAccounts,
                         ynabScheduledTransactions
                       );
-                    });
-                } else {
-                  this.props.addYNAB(
-                    { devToken: values.devToken, budgetId: values.budgetId },
-                    ynabAccounts,
-                    ynabScheduledTransactions
-                  );
-                }
-              })
-              .catch(error => {
-                console.log(error);
-              });
-            actions.setSubmitting(false);
-            actions.resetForm();
-          }}
-          render={({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting
-          }) => (
-            <form onSubmit={handleSubmit} autoComplete="off">
-              <div className="field is-horizontal">
-                <div className="field-label is-normal">
-                  <label className="label">Developer Token</label>
-                </div>
-                <div className="field-body">
-                  <div className="field">
-                    <p className="control">
-                      <Field type="text" name="devToken" className="input" />
-                    </p>
+                    }
+                  })
+                  .catch(error => {
+                    console.log(error);
+                  });
+                actions.setSubmitting(false);
+                actions.resetForm();
+              }}
+              render={({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting
+              }) => (
+                <form onSubmit={handleSubmit} autoComplete="off">
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">Developer Token</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            type="text"
+                            name="devToken"
+                            className="input"
+                          />
+                        </p>
+                      </div>
+                    </div>
+                    {touched.devToken && errors.devToken && (
+                      <div>{errors.devToken}</div>
+                    )}
                   </div>
-                </div>
-                {touched.devToken &&
-                  errors.devToken && <div>{errors.devToken}</div>}
-              </div>
-              <div className="field is-horizontal">
-                <div className="field-label is-normal">
-                  <label className="label">Budget ID</label>
-                </div>
-                <div className="field-body">
-                  <div className="field">
-                    <p className="control">
-                      <Field type="text" name="budgetId" className="input" />
-                    </p>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">Budget ID</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            type="text"
+                            name="budgetId"
+                            className="input"
+                          />
+                        </p>
+                      </div>
+                    </div>
+                    {touched.budgetId && errors.budgetId && (
+                      <div>{errors.budgetId}</div>
+                    )}
                   </div>
-                </div>
-                {touched.budgetId &&
-                  errors.budgetId && <div>{errors.budgetId}</div>}
-              </div>
-              <div className="field is-horizontal">
-                <div className="field-label is-normal">
-                  <label className="checkbox">
-                    <Field type="checkbox" name="importTransactions" /> Import
-                    Transactions Too (Recommend Only Doing This Initially)
-                  </label>
-                </div>
-                {touched.importTransactions &&
-                  errors.importTransactions && (
-                    <div>{errors.importTransactions}</div>
-                  )}
-              </div>
-              <div className="field is-grouped is-grouped-centered">
-                <div className="control">
-                  <button
-                    className="button is-link"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    Load Accounts
-                  </button>
-                </div>
-              </div>
-            </form>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="checkbox">
+                        <Field type="checkbox" name="importTransactions" />{' '}
+                        Import Transactions Too (Recommend Only Doing This
+                        Initially)
+                      </label>
+                    </div>
+                    {touched.importTransactions &&
+                      errors.importTransactions && (
+                        <div>{errors.importTransactions}</div>
+                      )}
+                  </div>
+                  <div className="field is-grouped is-grouped-centered">
+                    <div className="control">
+                      <button
+                        className="button is-link"
+                        type="submit"
+                        disabled={isSubmitting}
+                      >
+                        Load Accounts
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              )}
+            />
           )}
-        />
+        </Consumer>
       </React.Fragment>
     );
   }
@@ -224,4 +242,4 @@ const consumeYNABRepeat = (transaction, scheduledArray) => {
   return ynabSanitizedFrequency;
 };
 
-const parseDate = stringDate => parse(stringDate, 'YYYY-MM-DD', new Date());
+const parseDate = stringDate => parse(stringDate, 'yyyy-MM-dd', new Date());
