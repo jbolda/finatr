9066cdb1033cf41bacda3bf9f0bc31c310e91e95
Jacob Bolda <me@jacobbolda.com>
2019-03-13 08:16:58 -0500

    grouped components based on new data model

--- src/taxes.js
+++ src/taxes.js
@@ -26,30 +26,85 @@ const Taxes = props => {
 
 export default Taxes;
 
-const Group = ({ model }) => (
-  <div className="columns">
-    {map(model.taxStrategy.incomeReceived, income => (
-      <div className="column is-one-quarter" key={income.id.state}>
-        <div className="card">
-          <div className="card-content">
-            <p className="title">{income.group.state}</p>
-            <p>Gross: {income.gross.toFixed}</p>
-            <p>Federal: {income.federal.toFixed}</p>
-            <p>State: {income.state.toFixed}</p>
-            <p>Social Security: {income.socialSecurity.toFixed}</p>
-            <p>HSA: {income.hsa.toFixed}</p>
-            <p>Pretax: {income.pretaxInvestments.toFixed}</p>
-          </div>
-          <footer className="card-footer">
-            <p className="card-footer-item">
-              <span>Expand</span>
-            </p>
-          </footer>
+const Group = ({ model }) =>
+  map(model.taxStrategy.incomeGroup, group => (
+    <div key={group.name.state}>
+      <div className="columns">
+        <div className="column">
+          <p className="title">{group.name.state}</p>
         </div>
       </div>
-    ))}
-  </div>
-);
+      <div className="columns is-multiline">
+        <Chunk quarter="one" quarterIncome={group.income.qOne} />
+        <Chunk quarter="two" quarterIncome={group.income.qTwo} />
+        <Chunk quarter="three" quarterIncome={group.income.qThree} />
+        <Chunk quarter="four" quarterIncome={group.income.qFour} />
+      </div>
+    </div>
+  ));
+
+const Chunk = ({ quarter, quarterIncome }) => {
+  const [expanded, toggle] = useState(false);
+
+  return expanded ? (
+    <div className="column is-full">
+      <div className="card">
+        <div className="card-content">
+          <div className="level">
+            {quarterIncome.length !== 0 ? (
+              map(quarterIncome, income => (
+                <div
+                  className="level-item has-text-centered"
+                  key={income.id.state}
+                >
+                  <div>
+                    <p className="heading">{income.date.state}</p>
+                    <p>Gross: {income.gross.toFixed}</p>
+                    <p>Federal: {income.federalTax.toFixed}</p>
+                    <p>State: {income.stateTax.toFixed}</p>
+                    <p>Social Security: {income.socialSecurity.toFixed}</p>
+                    <p>HSA: {income.hsa.toFixed}</p>
+                    <p>Pretax: {income.pretaxInvestments.toFixed}</p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No items to display.</p>
+            )}
+          </div>
+        </div>
+        <footer className="card-footer">
+          <p className="card-footer-item">
+            <button
+              className="button is-white"
+              onClick={() => toggle(!expanded)}
+            >
+              Group
+            </button>
+          </p>
+        </footer>
+      </div>
+    </div>
+  ) : (
+    <div className="column is-one-quarter">
+      <div className="card">
+        <div className="card-content">
+          <p className="title">{quarter}</p>
+        </div>
+        <footer className="card-footer">
+          <p className="card-footer-item">
+            <button
+              className="button is-white"
+              onClick={() => toggle(!expanded)}
+            >
+              Expand
+            </button>
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
+};
 
 const Table = ({ model }) => (
   <table className="table is-striped is-hoverable">
