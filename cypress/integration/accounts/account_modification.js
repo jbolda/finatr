c426b64ff16275e4ca9783fbd7736519cee5008c
Jacob Bolda <me@jacobbolda.com>
2019-04-20 22:24:29 -0500

    add tests to check for account modifications

---
+++ cypress/integration/accounts/account_modification.js
@@ -0,0 +1,67 @@
+describe('Account Modifications Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#accounts')
+      .contains('Add Account')
+      .click();
+
+    cy.get('form')
+      .contains('starting')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}550');
+
+    cy.get('form')
+      .contains('name')
+      .parent()
+      .parent()
+      .find('input')
+      .type('test account');
+
+    cy.get('form').submit();
+    cy.get('#accounts')
+      .contains('550.00')
+      .parent()
+      .contains('M')
+      .click();
+  });
+
+  it('switches back to the form', () => {
+    cy.contains('Add an Account');
+  });
+
+  it('submits modified account', () => {
+    cy.get('form')
+      .contains('starting')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}5996');
+
+    cy.get('form').submit();
+    cy.get('#accounts').contains('5996.00');
+  });
+
+  it('check debt is listed in debt tab after submit', () => {
+    cy.get('form')
+      .contains('starting')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}577');
+
+    cy.get('form')
+      .contains('vehicle')
+      .parent()
+      .parent()
+      .find('select')
+      .select('Debt');
+
+    cy.get('form').submit();
+    cy.get('#accounts')
+      .contains('Debt')
+      .click();
+    cy.get('#accounts').contains('test account');
+  });
+});
