c1ffac5b8005f521c40c09d7a9f0999742ea3df5
Jacob Bolda <me@jacobbolda.com>
2019-04-22 07:21:27 -0500

    add account delete test

---
+++ cypress/integration/accounts/account_delete.js
@@ -0,0 +1,36 @@
+describe('Transaction Delete Tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#accounts')
+      .contains('Add Account')
+      .click();
+
+    cy.get('form')
+      .contains('starting')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}55');
+
+    cy.get('form')
+      .contains('name')
+      .parent()
+      .parent()
+      .find('input')
+      .type('test account');
+
+    cy.get('form').submit();
+  });
+
+  it('deletes the recently added transaction', () => {
+    cy.get('#accounts')
+      .contains('55.00')
+      .parent()
+      .within(() => {
+        cy.get('.delete').click();
+      });
+
+    const deletedAccount = cy.get('#accounts').contains('test account');
+    expect(deletedAccount).to.not.exist;
+  });
+});
