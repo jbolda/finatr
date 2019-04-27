641e0068ae24d7e6e60df8f8504046bb7e703753
Jacob Bolda <me@jacobbolda.com>
2019-04-27 16:02:42 -0500

    map in paybacks in account data structure

--- src/state/accounts.js
+++ src/state/accounts.js
@@ -1,11 +1,22 @@
 import { valueOf, StringType, BooleanType } from 'microstates';
 import { Big } from './customTypes.js';
+import { Transaction } from './transactions.js';
+import { create } from 'domain';
+
+class TransactionPayback extends Transaction {
+  debtAccount = StringType;
+}
+
+class AccountPayback {
+  transactions = create([TransactionPayback], [{}]);
+}
 
 class Account {
   name = StringType;
   starting = Big;
   interest = Big;
   vehicle = StringType;
+  payback = AccountPayback;
 
   get state() {
     return valueOf(this);
