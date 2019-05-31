3182c59004adc0236ee3c9f0af65a2b557f4cbea
Jacob Bolda <me@jacobbolda.com>
2019-05-31 07:14:06 -0500

    try PureComponent on charts for performance

--- src/pages/accounts/barChart.js
+++ src/pages/accounts/barChart.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import * as d3 from 'd3';
 
-export class BarChartSpecific extends Component {
+export class BarChartSpecific extends PureComponent {
   componentDidMount() {
     let svgBar = barBuild.init(
       350,
