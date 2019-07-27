ae2adb9e160fb8791383a20b1d09bd388f2d9ddf
Jacob Bolda <me@jacobbolda.com>
2019-07-27 11:12:56 -0500

    account form field updates

--- src/pages/flow/accountInput.js
+++ src/pages/flow/accountInput.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { State } from '../../state';
 import { Formik, Field } from 'formik';
+import { FieldGroup } from '../../components/bootstrap/Form';
 
 class AccountInput extends React.Component {
   render() {
@@ -33,79 +34,29 @@ class AccountInput extends React.Component {
                 isSubmitting
               }) => (
                 <form onSubmit={handleSubmit} autoComplete="off">
-                  <div className="field is-horizontal">
-                    <div className="field-label is-normal">
-                      <label className="label">name</label>
+                  <FieldGroup errors={errors} name="name" touched={touched}>
+                    <Field type="text" name="name" className="input" />
+                  </FieldGroup>
+
+                  <FieldGroup errors={errors} name="starting" touched={touched}>
+                    <Field type="number" name="starting" className="input" />
+                  </FieldGroup>
+
+                  <FieldGroup errors={errors} name="interest" touched={touched}>
+                    <Field type="number" name="interest" className="input" />
+                  </FieldGroup>
+
+                  <FieldGroup errors={errors} name="vehicle" touched={touched}>
+                    <div className="select">
+                      <Field component="select" name="vehicle">
+                        <option value="operating">Operating</option>
+                        <option value="loan">Loan</option>
+                        <option value="credit line">Credit Line</option>
+                        <option value="investment">Investment</option>
+                      </Field>
                     </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <Field type="text" name="name" className="input" />
-                        </p>
-                      </div>
-                    </div>
-                    {touched.name && errors.name && <div>{errors.name}</div>}
-                  </div>
-                  <div className="field is-horizontal">
-                    <div className="field-label is-normal">
-                      <label className="label">starting</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <Field
-                            type="number"
-                            name="starting"
-                            className="input"
-                          />
-                        </p>
-                      </div>
-                    </div>
-                    {touched.starting && errors.starting && (
-                      <div>{errors.starting}</div>
-                    )}
-                  </div>
-                  <div className="field is-horizontal">
-                    <div className="field-label is-normal">
-                      <label className="label">interest</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <p className="control">
-                          <Field
-                            type="number"
-                            name="interest"
-                            className="input"
-                          />
-                        </p>
-                      </div>
-                    </div>
-                    {touched.interest && errors.interest && (
-                      <div>{errors.interest}</div>
-                    )}
-                  </div>
-                  <div className="field is-horizontal">
-                    <div className="field-label is-normal">
-                      <label className="label">vehicle</label>
-                    </div>
-                    <div className="field-body">
-                      <div className="field">
-                        <div className="control">
-                          <div className="select">
-                            <Field component="select" name="vehicle">
-                              <option value="operating">Operating</option>
-                              <option value="loan">Loan</option>
-                              <option value="credit line">Credit Line</option>
-                              <option value="investment">Investment</option>
-                            </Field>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    {touched.vehicle && errors.vehicle && (
-                      <div>{errors.vehicle}</div>
-                    )}
-                  </div>
+                  </FieldGroup>
+
                   <div className="field is-grouped is-grouped-centered">
                     <div className="control">
                       <button
