3a4a0d2badc52c5cd9b278842a63516bc1512529
Jacob Bolda <me@jacobbolda.com>
2019-07-28 00:37:42 -0500

    CI weirdness (#100)
    * increase wait
    
    * remove wait
    
    * remove all waits
    
    * try running beforeEach after load
    
    * grr IDE import
    
    * try lower version of formik
    
    * the lockfile didn't come along for the ride
    
    * and the npm lock
    
    * onLoad doesn't work...

--- cypress/integration/accounts/account_modification.js
+++ cypress/integration/accounts/account_modification.js
@@ -1,8 +1,6 @@
 describe('Account Modifications Tests', () => {
   beforeEach(() => {
-    cy.wait(100);
     cy.visit('/flow');
-    cy.wait(100);
     cy.get('#accounts')
       .contains('Add Account')
       .click();
