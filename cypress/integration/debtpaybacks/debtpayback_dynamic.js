407510857381c1f372b3ec0dc4cd331ca33f17cc
Jacob Bolda <me@jacobbolda.com>
2019-08-16 08:09:52 -0500

    add test triggering references validation

--- cypress/integration/debtpaybacks/debtpayback_dynamic.js
+++ cypress/integration/debtpaybacks/debtpayback_dynamic.js
@@ -91,4 +91,74 @@ describe('Debt Payback Form Tests', () => {
 
     cy.get('#accounts').contains('1 for 0');
   });
+
+  it('validates on non-entered references', () => {
+    cy.get('#accounts')
+      .contains('+')
+      .click();
+
+    cy.get('form')
+      .contains('debt account')
+      .parent()
+      .parent()
+      .find('select')
+      .select('Test Debt Submission');
+    cy.get('form')
+      .contains('payment account')
+      .parent()
+      .parent()
+      .find('select')
+      .select('account');
+    cy.get('form')
+      .contains('rtype')
+      .parent()
+      .parent()
+      .find('select')
+      .select('Repeat on a Day of the Week');
+    cy.get('form')
+      .contains('start')
+      .parent()
+      .parent()
+      .find('input')
+      .type('2019-04-28');
+    cy.get('form')
+      .contains('occurrences')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}8');
+    cy.get('form')
+      .contains('cycle')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}1');
+
+    cy.get('form')
+      .contains('Dynamic')
+      .find('input')
+      .check();
+    cy.get('form')
+      .contains('reference name')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}Special Balance');
+    cy.get('form')
+      .contains('reference value')
+      .parent()
+      .parent()
+      .find('input')
+      .type('{selectall}250');
+    cy.get('form').submit();
+
+    cy.get('form')
+      .contains('reference')
+      .parent()
+      .parent()
+      .parent()
+      .should(refSection => {
+        expect(refSection).to.contain('Required');
+      });
+  });
 });
