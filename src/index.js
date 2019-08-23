47fb78a25e4ecda502cdae5101b3042468606531
Jacob Bolda <me@jacobbolda.com>
2019-08-22 22:40:19 -0500

    switch out bulma for theme-ui and rebass (#120)
    * switch packages in/out
    
    * replace nav
    
    * add css-in-js branch to renovate as may be long running branch
    
    * update package.lock

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { Router, Link } from '@reach/router';
 import { Store, create } from 'microstates';
 import AppModel, { State } from './state';
 
-import 'bulma/css/bulma.css';
+import { ThemeProvider } from 'theme-ui';
+import { Flex, Box, Text } from 'rebass';
+import theme from '@rebass/preset';
+
 import Homepage from './pages/homepage';
 import Examples from './pages/examples';
 import Financial from './pages/flow';
@@ -16,87 +19,52 @@ import Taxes from './pages/taxes';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.toggleHamburgerMenu = this.toggleHamburgerMenu.bind(this);
     this.state = {
-      hamburgerActive: false,
       model: Store(create(AppModel, AppModel), nextState =>
         this.setState({ model: nextState })
       )
     };
   }
 
-  toggleHamburgerMenu() {
-    this.setState(prevState => {
-      return { hamburgerActive: !prevState.hamburgerActive };
-    });
-  }
-
   render() {
     return (
       <State.Provider value={this.state.model}>
-        <nav
-          className="navbar is-fixed-top is-primary"
-          role="navigation"
-          aria-label="main navigation"
-        >
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item">
-              finatr
-            </Link>
-            {/* eslint-disable-next-line */}
-            <a
-              role="button"
-              className={
-                this.state.hamburgerActive
-                  ? 'navbar-burger is-active'
-                  : 'navbar-burger'
-              }
-              aria-label="menu"
-              aria-expanded={this.state.hamburgerActive ? 'true' : 'false'}
-              onClick={this.toggleHamburgerMenu}
-            >
-              <span aria-hidden="true" />
-              <span aria-hidden="true" />
-              <span aria-hidden="true" />
-            </a>
-          </div>
-          <div
-            className={
-              this.state.hamburgerActive
-                ? 'navbar-menu is-active'
-                : 'navbar-menu'
-            }
-          >
-            <div className="navbar-end">
-              <Link to="/" className="navbar-item">
-                Home
-              </Link>
-              <Link to="examples" className="navbar-item">
-                Examples
-              </Link>
-              <Link to="flow" className="navbar-item">
-                Cash Flow
-              </Link>
-              <Link to="accounts" className="navbar-item">
-                Accounts
-              </Link>
-              <Link to="import" className="navbar-item">
-                Import
-              </Link>
-              <Link to="taxes" className="navbar-item">
-                Taxes
-              </Link>
-            </div>
-          </div>
-        </nav>
-        <Router>
-          <Homepage path="/" />
-          <Examples path="examples" />
-          <Financial path="flow" />
-          <Accounts path="accounts" />
-          <Importing path="import" />
-          <Taxes path="taxes" />
-        </Router>
+        <ThemeProvider theme={theme}>
+          <Flex px={2} color="white" bg="black" alignItems="center">
+            <Text p={2} fontWeight="bold">
+              {' '}
+              <Link to="/">finatr</Link>
+            </Text>
+            <Box mx="auto" />
+            <Text p={2}>
+              <Link to="/">Home</Link>
+            </Text>
+            <Text p={2}>
+              <Link to="examples">Examples</Link>
+            </Text>
+            <Text p={2}>
+              <Link to="flow">Cash Flow</Link>
+            </Text>
+            <Text p={2}>
+              <Link to="accounts">Accounts</Link>
+            </Text>
+            <Text p={2}>
+              <Link to="import">Import</Link>
+            </Text>
+            <Text p={2}>
+              <Link to="taxes">Taxes</Link>
+            </Text>
+          </Flex>
+
+          <Router>
+            <Homepage path="/" />
+            <Examples path="examples" />
+            <Financial path="flow" />
+            <Accounts path="accounts" />
+            <Importing path="import" />
+            <Taxes path="taxes" />
+          </Router>
+        </ThemeProvider>
       </State.Provider>
     );
   }
