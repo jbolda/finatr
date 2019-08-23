9862699c36ccc28bfcdce6d7be5d2c2844d4e3a2
Jacob Bolda <me@jacobbolda.com>
2019-08-23 08:57:21 -0500

    switch to react/abs for TabView

--- src/components/view/tabView.js
+++ src/components/view/tabView.js
@@ -1,77 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
+import '@reach/tabs/styles.css';
 
-class TabView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeTab: null };
-    if (props.tabTitles.length > 0) {
-      this.state.activeTab = 0;
-    }
-  }
-
-  tabClick(index, event) {
-    if (event) event.preventDefault();
-    if (this.props.tabClick) {
-      this.props.tabClick(index);
-    } else {
-      this.setState({ activeTab: index });
-    }
-  }
-
-  computeUrl(tabName) {
-    // href={`#/${this.computeUrl(tab)}`}
-    let url = tabName.toLowerCase();
-    url = url.replace(' ', '_');
-    return url;
-  }
-
-  render() {
-    let tabContents = null;
-    let activeTab = this.props.activeTab
-      ? this.props.activeTab
-      : this.state.activeTab;
-
-    if (
-      activeTab !== null &&
-      this.props.tabContents &&
-      this.props.tabContents[activeTab]
-    ) {
-      tabContents = this.props.tabContents[activeTab];
-    }
-
-    return (
-      <React.Fragment>
-        <div className="tabs">
-          <ul>
-            {this.props.tabTitles.map((tab, index) => (
-              <li key={tab} className={index === activeTab ? 'is-active' : ''}>
-                <a
-                  href={`#/${this.computeUrl(tab)}`}
-                  onClick={this.tabClick.bind(this, index)}
-                >
-                  {tab}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="container is-fluid" style={{ overflowX: 'auto' }}>
-          {tabContents}
-        </div>
-      </React.Fragment>
-    );
-  }
-}
-
-TabView.defaultProps = {
-  tabTitles: [],
-  tabContents: null
-};
-
-TabView.propTypes = {
-  tabTitles: PropTypes.array,
-  tabContents: PropTypes.node
-};
+const TabView = ({ tabTitles, tabContents }) => (
+  <Tabs>
+    <TabList>
+      {tabTitles.map(title => (
+        <Tab>{title}</Tab>
+      ))}
+    </TabList>
+    <TabPanels>
+      {tabContents.map(content => (
+        <TabPanel>{content}</TabPanel>
+      ))}
+    </TabPanels>
+  </Tabs>
+);
 
 export default TabView;
