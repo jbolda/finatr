0c72a90e2a7cb3c0282839719fa444f2e543376c
Jacob Bolda <me@jacobbolda.com>
2019-08-24 09:06:21 -0500

    pad TabView slightly

--- src/components/view/tabView.js
+++ src/components/view/tabView.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Box } from 'rebass';
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
 import '@reach/tabs/styles.css';
 
 const TabView = ({ tabTitles, tabContents }) => (
-  <Tabs>
-    <TabList>
-      {tabTitles.map(title => (
-        <Tab key={title}>{title}</Tab>
-      ))}
-    </TabList>
-    <TabPanels>
-      {tabContents.map((content, index) => (
-        <TabPanel key={index}>{content}</TabPanel>
-      ))}
-    </TabPanels>
-  </Tabs>
+  <Box pt={4} pb={4}>
+    <Tabs>
+      <TabList>
+        {tabTitles.map(title => (
+          <Tab key={title}>{title}</Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {tabContents.map((content, index) => (
+          <TabPanel key={index}>{content}</TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  </Box>
 );
 
 export default TabView;
