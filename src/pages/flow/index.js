2687d345ba8d50c451bd274cecfdd6f623bf13eb
Jacob Bolda <me@jacobbolda.com>
2019-08-25 11:16:15 -0500

    add breakpoints to stats

--- src/pages/flow/index.js
+++ src/pages/flow/index.js
@@ -16,19 +16,19 @@ class FinancialFlow extends React.Component {
         {model => (
           <React.Fragment>
             <Flex flexWrap="wrap" mx={5}>
-              <Box px={2} py={2} width={1 / 5}>
+              <Box px={2} py={2} width={[1, 1 / 5, null]}>
                 <Text p={1}>Daily Income</Text>
                 <Text p={1}>${model.stats.dailyIncome.toFixed}</Text>
               </Box>
-              <Box px={2} py={2} width={1 / 5}>
+              <Box px={2} py={2} width={[1, 1 / 5, null]}>
                 <Text p={1}>Daily Expenses</Text>
                 <Text p={1}>${model.stats.dailyExpense.toFixed}</Text>
               </Box>
-              <Box px={2} py={2} width={1 / 5}>
+              <Box px={2} py={2} width={[1, 1 / 5, null]}>
                 <Text p={1}>Savings Rate</Text>
                 <Text p={1}>{model.stats.savingsRate.toFixed}%</Text>
               </Box>
-              <Box px={2} py={2} width={1 / 5}>
+              <Box px={2} py={2} width={[1, 1 / 5, null]}>
                 <Text p={1}>Expense Multiple</Text>
                 <Text p={1}>{model.stats.expenseMultiple.toFixed}x</Text>
                 <Text p={1}>
@@ -36,7 +36,7 @@ class FinancialFlow extends React.Component {
                   x/year
                 </Text>
               </Box>
-              <Box px={2} py={2} width={1 / 5}>
+              <Box px={2} py={2} width={[1, 1 / 5, null]}>
                 <Text p={1}>% to FI (25x)</Text>
                 <Text p={1}>{model.stats.percentToFINumber.toFixed}%</Text>
                 {model.stats.yearsToFINumber.toFixed === '999.00' ? null : (
@@ -44,13 +44,13 @@ class FinancialFlow extends React.Component {
                 )}
               </Box>
 
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to +net worth</Text>
                 <Text p={1}>
                   {model.stats.percentToPositiveNetWorth.toFixed}%
                 </Text>
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to FU Money (2x)</Text>
                 <Text p={1}>
                   {model.stats.percentToFUMoneyConsidering.toFixed}%
@@ -62,7 +62,7 @@ class FinancialFlow extends React.Component {
                   </Text>
                 )}
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to FU Money (3x)</Text>
                 <Text p={1}>
                   {model.stats.percentToFUMoneyConfident.toFixed}%
@@ -74,33 +74,33 @@ class FinancialFlow extends React.Component {
                   </Text>
                 )}
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to first FI milestone</Text>
                 <Text p={1}>($100,000 Net Worth)</Text>
                 <Text p={1}>{model.stats.percentToFirstFI.toFixed}%</Text>
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to half FI (12.5x)</Text>
                 <Text p={1}>{model.stats.percentToHalfFI.toFixed}%</Text>
                 {model.stats.yearsToHalfFI.toFixed === '999.00' ? null : (
                   <Text p={1}>{model.stats.yearsToHalfFI.toFixed}y</Text>
                 )}
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to lean FI (17.5x)</Text>
                 <Text p={1}>{model.stats.percentToLeanFI.toFixed}%</Text>
                 {model.stats.yearsToLeanFI.toFixed === '999.00' ? null : (
                   <Text p={1}>{model.stats.yearsToLeanFI.toFixed}y</Text>
                 )}
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to flex FI (20x)</Text>
                 <Text p={1}>{model.stats.percentToFlexFI.toFixed}%</Text>
                 {model.stats.yearsToFlexFI.toFixed === '999.00' ? null : (
                   <Text p={1}>{model.stats.yearsToFlexFI.toFixed}y</Text>
                 )}
               </Box>
-              <Box px={2} py={2} width={1 / 8}>
+              <Box px={2} py={2} width={[1, 1 / 8, null]}>
                 <Text p={1}>% to fat FI (30x)</Text>
                 <Text p={1}>{model.stats.percentToFatFI.toFixed}%</Text>
                 {model.stats.yearsToFatFI.toFixed === '999.00' ? null : (
