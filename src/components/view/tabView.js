1ced4303353f30fdffd99eab51ddbfaa5bfea97b
Jacob Bolda <me@jacobbolda.com>
2019-08-27 08:04:44 -0500

    switch TabView back to class for easier testing for now

--- src/components/view/tabView.js
+++ src/components/view/tabView.js
@@ -3,21 +3,47 @@ import { Box } from 'rebass';
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
 import '@reach/tabs/styles.css';
 
-const TabView = ({ tabTitles, tabContents }) => (
-  <Box pt={4} pb={4}>
-    <Tabs>
-      <TabList>
-        {tabTitles.map(title => (
-          <Tab key={title}>{title}</Tab>
-        ))}
-      </TabList>
-      <TabPanels>
-        {tabContents.map((content, index) => (
-          <TabPanel key={index}>{content}</TabPanel>
-        ))}
-      </TabPanels>
-    </Tabs>
-  </Box>
-);
+class TabView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: null };
+    if (!!props.tabTitles && props.tabTitles.length > 0) {
+      this.state.activeTab = 0;
+    }
+  }
+
+  tabClick(index) {
+    if (this.props.tabClick) {
+      this.props.tabClick(index);
+    } else {
+      this.setState({ activeTab: index });
+    }
+  }
+
+  render() {
+    let activeTab = this.props.activeTab
+      ? this.props.activeTab
+      : this.state.activeTab;
+
+    return (
+      <Box pt={4} pb={4}>
+        {!this.props.tabTitles || !this.props.tabContents ? null : (
+          <Tabs index={activeTab} onChange={index => this.tabClick(index)}>
+            <TabList>
+              {this.props.tabTitles.map(title => (
+                <Tab key={title}>{title}</Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              {this.props.tabContents.map((content, index) => (
+                <TabPanel key={index}>{content}</TabPanel>
+              ))}
+            </TabPanels>
+          </Tabs>
+        )}
+      </Box>
+    );
+  }
+}
 
 export default TabView;
