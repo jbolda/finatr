5a79965b92da250da63af73bcbe873a51688bcbc
Jacob Bolda <me@jacobbolda.com>
2019-08-31 22:04:53 -0500

    refine theme, add color modes

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,35 @@
 export default {
   breakpoints: ['40em', '52em', '64em'],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  initialColorMode: 'light',
+  useCustomProperties: true,
   colors: {
-    blue: '#07c',
-    lightgray: '#f6f6ff'
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#07c',
+    muted: '#07c',
+    accent: '#07c',
+    modes: {
+      dark: {
+        text: '#e0dfdf',
+        background: '#0c0c0c',
+        primary: '#0cf',
+        secondary: '#07c',
+        muted: '#07c',
+        accent: '#07c'
+      }
+    }
   },
   space: [0, 4, 8, 16, 32, 64, 128, 256],
   fonts: {
-    body: 'system-ui, sans-serif',
-    heading: 'inherit',
+    body: 'Libre Baskerville, serif',
+    heading: 'Raleway, sans-serif',
     monospace: 'Menlo, monospace'
   },
   fontWeights: {
     body: 400,
-    heading: 700,
+    heading: 750,
     bold: 700
   },
   lineHeights: {
