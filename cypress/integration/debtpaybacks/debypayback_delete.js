af55111ba0892981f2c2c9d0e3607959875e7f03
Jacob Bolda <me@jacobbolda.com>
2019-09-04 21:48:57 -0500

    debt payback delete passes

--- cypress/integration/debtpaybacks/debypayback_delete.js
+++ cypress/integration/debtpaybacks/debypayback_delete.js
@@ -4,91 +4,47 @@ describe('Debt Payback Form Tests', () => {
     cy.get('#accounts')
       .contains('Add Account')
       .click();
-    cy.get('form')
-      .contains('name')
-      .parent()
-      .parent()
-      .find('input')
-      .type('Test Debt Submission');
-    cy.get('form')
-      .contains('vehicle')
-      .parent()
-      .parent()
-      .find('select')
-      .select('Loan');
-    cy.get('form')
-      .contains('starting')
-      .parent()
-      .parent()
-      .find('input')
-      .type('{selectall}20000');
-    cy.get('form').submit();
 
-    cy.get('#accounts')
-      .contains('Debt')
-      .click();
+    cy.get('#accounts').within(() => {
+      cy.getByLabelText('name').type('Test Debt Submission');
 
-    cy.get('#accounts')
-      .contains('+')
-      .click();
+      cy.getByLabelText('vehicle').select('Loan');
 
-    cy.get('form')
-      .contains('debt account')
-      .parent()
-      .parent()
-      .find('select')
-      .select('Test Debt Submission');
-    cy.get('form')
-      .contains('payment account')
-      .parent()
-      .parent()
-      .find('select')
-      .select('account');
-    cy.get('form')
-      .contains('rtype')
-      .parent()
-      .parent()
-      .find('select')
-      .select('No Repeating');
-    cy.get('form')
-      .contains('value')
-      .parent()
-      .parent()
-      .find('input')
-      .type('55');
-    cy.get('form')
-      .contains('start')
-      .parent()
-      .parent()
-      .find('input')
-      .type('2019-05-28');
-    cy.get('form')
-      .contains('occurrences')
-      .parent()
-      .parent()
-      .find('input')
-      .type('8');
-    cy.get('form')
-      .contains('cycle')
-      .parent()
-      .parent()
-      .find('input')
-      .type('1');
-    cy.get('form').submit();
+      cy.getByLabelText('starting').type('{selectall}20000');
+
+      cy.get('form').submit();
+
+      cy.getByText('Debt').click();
+
+      cy.getByText('+').click();
+
+      cy.getByLabelText('debt account').select('Test Debt Submission');
+
+      cy.getByLabelText('payment account').select('account');
+
+      cy.getByLabelText('rtype').select('No Repeating');
+
+      cy.getByLabelText('value').type('55');
+
+      cy.getByLabelText('start').type('2019-05-28');
+
+      cy.getByText('after Number of Occurrences').click();
+
+      cy.getByLabelText('occurrences').type('8');
+
+      cy.getByLabelText('cycle').type('1');
+
+      cy.getByTestId('accounts-debt').within(() => cy.get('form').submit());
+    });
   });
 
   it('deletes the recently added debt payback', () => {
-    cy.get('#accounts')
-      .contains('2019-05-28')
-      .parent()
-      .parent()
-      .parent()
-      .within(() => {
-        cy.contains('X').click();
-      });
+    cy.getByTestId('accounts-debt').within(() => {
+      cy.getByText('2019-05-28')
+        .parent()
+        .within(() => cy.getByText('X').click());
 
-    cy.get('#accounts')
-      .contains('2019-05-28')
-      .should('not.exist');
+      cy.queryByText('2019-05-28').should('not.exist');
+    });
   });
 });
