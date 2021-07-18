1485108d2a610768f078c21ad0fccb8e4732674a
Jacob Bolda <me@jacobbolda.com>
2021-07-18 02:08:20 -0500

    rough out common components

--- src/components/common/link.js
+++ src/components/common/link.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Link as LinkThemed, NavLink as NavLinkThemed } from 'theme-ui';
-import { Link as LinkNavigation } from '@reach/router';
+import { Link as RRLink, NavLink as RRNavLink } from 'react-router-dom';
 
-export default (props) => {
-  return (
-    <LinkThemed to={props.to} as={LinkNavigation} variant="links.primary">
-      {props.children}
-    </LinkThemed>
-  );
+const Link = ({ to, children }) => {
+  return <RRLink to={to}>{children}</RRLink>;
 };
 
-const NavLink = (props) => {
-  return (
-    <NavLinkThemed to={props.to} as={LinkNavigation} variant="links.nav">
-      {props.children}
-    </NavLinkThemed>
-  );
+const NavLink = ({ to, children }) => {
+  return <RRNavLink to={to}>{children}</RRNavLink>;
 };
 
-export { NavLink };
+export { Link, NavLink };
