56ea8586feed24dddf09badee25bafa833391d5f
Jacob Bolda <me@jacobbolda.com>
2021-07-18 02:11:22 -0500

    rough out account page

--- src/pages/accounts/index.js
+++ src/pages/accounts/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { State } from '../../state';
 import BarChart from './barChart';
-import { Flex, Box, Heading } from 'theme-ui';
 
 const AccountInfo = (props) => (
   <State.Consumer>
-    {(model) =>
-      model.charts.state.AccountChart.map((account) => (
-        <Flex key={account.account.name}>
-          <Box m={2}>
-            <Heading variant="subtle">{account.account.name}</Heading>
-            <BarChart data={model.charts.state} account={account} />
-          </Box>
-        </Flex>
-      ))
-    }
+    {(model) => (
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-semibold py-3">Cash Flow By Account</h1>
+        {model.charts.state.AccountChart.map((account) => (
+          <div className="bg-white my-2 py-1 overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+            <div className="px-4 py-5 sm:px-6">
+              <h2 className="text-2xl font-semibold">{account.account.name}</h2>
+            </div>
+            <div className="px-4 py-5 sm:p-6">
+              <BarChart data={model.charts.state} account={account} />
+            </div>
+          </div>
+        ))}
+      </div>
+    )}
   </State.Consumer>
 );
 
