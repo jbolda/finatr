1afa8eddac780d6ff542d5755cf6f4e5c91cc387
Jacob Bolda <me@jacobbolda.com>
2021-08-30 00:57:08 -0500

    add back in FlexTable feature

--- src/components/FlexTable.js
+++ src/components/FlexTable.js
@@ -1,12 +1,11 @@
 import React from 'react';
-const Box = ({ children }) => <div>{children}</div>;
 
 const HeaderRow = ({ items, columns }) => (
-  <tr>
+  <tr className="divide-x hidden 2xl:table-row">
     {items.map((item) => (
       <th
         scope="col"
-        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        className="px-5 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider"
         key={item}
       >
         {item}
@@ -15,34 +14,18 @@ const HeaderRow = ({ items, columns }) => (
   </tr>
 );
 
-const HeaderItem = ({ children }) => (
-  <Box
-    px={0}
-    sx={{
-      display: ['none', 'inline', 'inline']
-    }}
-  >
-    {children}
-  </Box>
-);
-
 const DataRow = ({ items, columns, itemKey, itemHeaders }) => (
-  <tr key={itemKey}>
+  <tr key={itemKey} className="divide-x">
     {items.map((item, index) => (
-      <td key={`${itemKey}=${index}`} className="px-6 py-4 whitespace-nowrap">
-        {item}
+      <td
+        key={`${itemKey}=${index}`}
+        className="px-5 py-1 2xl:py-4 whitespace-nowrap block 2xl:table-cell"
+      >
+        <span className="2xl:hidden">{`${itemHeaders[index]}: `}</span>
+        <span>{item}</span>
       </td>
     ))}
   </tr>
-);
-
-const DataItem = ({ children, header }) => (
-  <React.Fragment>
-    <Box px={0} sx={{ display: ['inline', 'none', 'none'] }}>
-      {header}
-    </Box>
-    <Box px={0}>{children}</Box>
-  </React.Fragment>
 );
 
 export { HeaderRow, DataRow };
@@ -53,7 +36,7 @@ export const FlexTable = ({ itemHeaders, itemData }) => (
       <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
+            <thead className="bg-gray-50 text-left">
               <HeaderRow columns={itemHeaders.length} items={itemHeaders} />
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
