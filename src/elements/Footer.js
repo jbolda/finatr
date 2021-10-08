7706a83361f11159a6e28105184e7f37c1a1c892
Jacob Bolda <me@jacobbolda.com>
2021-10-08 09:37:41 -0500

    remove extraneous footer items (#454)
    * remove extraneous footer items
    
    * change file

--- src/elements/Footer.js
+++ src/elements/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Heart = () => (
   <span role="img" aria-label="heart symbol">
-    ❤️️️
+    {' *❤️  * '}
   </span>
 );
 
@@ -11,14 +11,15 @@ export const Footer = (props) => {
     <footer className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <div className="text-base text-gray-500 hover:text-gray-900 flex justify-center">
-          <span>
-            Made with <Heart /> by{' '}
-            <a href="https://www.jacobbolda.com">Jacob Bolda</a> and{' '}
+          <p>
+            Made with
+            <Heart />
+            by <a href="https://www.jacobbolda.com">Jacob Bolda</a> and{' '}
             <a href="https://github.com/jbolda/finatr/graphs/contributors">
               all our contributors
             </a>
             .
-          </span>
+          </p>
         </div>
         <nav
           className="-mx-5 my-2 flex flex-wrap justify-center"
@@ -27,7 +28,7 @@ export const Footer = (props) => {
           {navigation.main.map((item) => (
             <div key={item.name} className="px-5 py-2">
               <a
-                href={item.href}
+                href={`/${item.to}`}
                 className="text-base text-gray-500 hover:text-gray-900"
               >
                 {item.name}
@@ -39,7 +40,7 @@ export const Footer = (props) => {
           {navigation.social.map((item) => (
             <a
               key={item.name}
-              href={item.href}
+              href={item.to}
               className="text-gray-400 hover:text-gray-500"
             >
               <span className="sr-only">{item.name}</span>
@@ -62,17 +63,14 @@ export const Footer = (props) => {
 
 const navigation = {
   main: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Accessibility', href: '#' },
-    { name: 'Partners', href: '#' }
+    { name: 'Home', to: '' },
+    { name: 'Cash Flow', to: 'flow' },
+    { name: 'Import', to: 'import' }
   ],
   social: [
     {
       name: 'Twitter',
-      href: '#',
+      to: 'https://www.twitter.com/jacobbolda',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -81,7 +79,7 @@ const navigation = {
     },
     {
       name: 'GitHub',
-      href: '#',
+      to: 'https://github.com/jbolda/finatr',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
