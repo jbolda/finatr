bcbd319b6d2811d5583dd9dcbdda8ddba9d060b9
Jacob Bolda <me@jacobbolda.com>
2023-10-07 21:21:16 -0500

    react-aria conversion with Tab component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '~src/app.module.css';
 
 import { Store, create } from 'microstates';
 import AppModel, { State } from './state';
