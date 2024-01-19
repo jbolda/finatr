a2c0ed0b1ff9039feeef3c4eea930b15a07f51a6
Jacob Bolda <me@jacobbolda.com>
2024-01-18 23:33:00 -0600

    bump react and add starfx

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = async () => {
-  return {
-    testEnvironment: 'node',
-    resolver: '<rootDir>/jest.resolver.js',
-    testPathIgnorePatterns: ['<rootDir>/tests/', '<rootDir>/node_modules/']
-  };
-};
