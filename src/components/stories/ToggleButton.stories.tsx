cd8e954e7624677a09ff983af4d2c92795c6c178
Jacob Bolda <me@jacobbolda.com>
2024-01-22 00:59:04 -0600

    pull in react-aria tailwind starter kit

---
+++ src/components/stories/ToggleButton.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta } from '@storybook/react';
+import React from 'react';
+import { ToggleButton } from '../ToggleButton';
+
+const meta: Meta<typeof ToggleButton> = {
+  component: ToggleButton,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+
+export const Example = (args: any) => (
+  <ToggleButton {...args}>Pin</ToggleButton>
+);
