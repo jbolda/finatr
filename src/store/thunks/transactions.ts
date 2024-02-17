1ad174d4e6cefcdfc4ccd18ac05abe4c3628a1f7
Jacob Bolda <me@jacobbolda.com>
2024-02-17 08:43:49 -0600

    transaction dailyRate'

--- src/store/thunks/transactions.ts
+++ src/store/thunks/transactions.ts
@@ -9,6 +9,12 @@ export const transactionAdd = thunks.create(
   'transaction:add',
   function* (ctx, next) {
     const transactionID = makeUUID();
+    const transaction = { ...ctx.payload };
+    transaction.id = transactionID;
+    transaction.value = new Big(ctx.payload.value);
+    transaction.cycle = new Big(ctx.payload.cycle);
+    transaction.occurrences = new Big(ctx.payload.occurrences);
+    transaction.dailyRate = transactionCompute({ transaction });
 
     yield* schema.update(
       schema.transactions.add({ [transactionID]: transaction })
