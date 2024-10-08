59f5609063b7e3e9e872e1904dc6d4326346645e
Jacob Bolda <me@jacobbolda.com>
2024-10-08 11:06:00 -0500

    upgrade to Tauri v2 stable

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_shell::init())
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  finatr_lib::run()
 }
