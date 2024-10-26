60bc35c16e680d8e50f4786b072e07179f68ac26
Jacob Bolda <me@jacobbolda.com>
2024-10-26 13:31:06 -0500

    rough in filter for accounts, fix account calc (#838)
    * rough in filter for accounts, fix account calc
    
    * pill filter for specific accounts
    
    * radio filter scrolls
    
    * tighten up mobile padding
    
    * change file

--- src/pages/flow/barChart.tsx
+++ src/pages/flow/barChart.tsx
@@ -3,18 +3,30 @@ import { closestIndexTo, eachDayOfInterval } from 'date-fns';
 import React, { useEffect } from 'react';
 import { useSelector } from 'starfx/react';
 
-import { lineChartAccounts } from '~/src/store/selectors/accounts';
+import { type ChartAccounts } from '~/src/store/selectors/accounts';
 import { barChartTransactions } from '~/src/store/selectors/chartData';
 import { toHumanCurrency } from '~/src/store/utils/dineroUtils';
 
-const BarChart = ({ dateRange }) => {
+const BarChart = ({
+  dateRange,
+  accounts,
+  vehicleFilter
+}: {
+  dateRange: {
+    start: Date;
+    startString: string;
+    end: Date;
+    endString: string;
+  };
+  accounts: ChartAccounts['data'];
+  vehicleFilter: string;
+}) => {
   const transactionData = useSelector(barChartTransactions);
-  const accountData = useSelector(lineChartAccounts);
   const bar = barBuild;
 
   useEffect(() => {
-    drawCharts(dateRange, transactionData, accountData);
-  }, [transactionData.data, accountData.data, dateRange.start]);
+    drawCharts(dateRange, transactionData, accounts);
+  }, [transactionData.data, accounts, dateRange.start, vehicleFilter]);
 
   return (
     <>
@@ -93,14 +105,19 @@ const BarChart = ({ dateRange }) => {
 
 export default BarChart;
 
-const drawCharts = (dateRange, transactionData, accountData) => {
+const drawCharts = (
+  dateRange,
+  transactionData,
+  accounts: ChartAccounts['data']
+) => {
   const svgBar = d3.select('g.bar-section');
   const svgLine = d3.select('g.line-section');
   const tooltipTarget = d3.select('div#tooltipTarget').style('opacity', 0);
   const tooltipLine = d3.select('line#tooltipLine').style('opacity', 0);
+  const accountMax = d3.max(accounts, (d) => d3.max(d.data, (dd) => dd[1]));
 
   barBuild.drawAxis(svgBar, dateRange, transactionData.max);
-  barBuild.drawAxis(svgLine, dateRange, accountData.max);
+  barBuild.drawAxis(svgLine, dateRange, accountMax);
 
   barBuild.drawBar({
     selector: svgBar.select('.blobs'),
@@ -117,8 +134,8 @@ const drawCharts = (dateRange, transactionData, accountData) => {
     svg: svgLine.select('rect#mouseArea'),
     selector: svgLine.select('.lines'),
     dateRange,
-    data: accountData.data,
-    max_domain: accountData.max,
+    data: accounts,
+    max_domain: accountMax,
     tooltip: {
       target: tooltipTarget,
       render: renderTooltipLine,
@@ -202,223 +219,217 @@ let barBuild = {
         //  the minimum is second to reverse to top left SVG coordinates
         .range([this.height() - this.margin().top - this.margin().bottom, 0])
     );
-  }
-};
+  },
+  drawAxis: function (svg, dateRange, max_domain) {
+    // create axis
+    let xAxis = d3
+      .axisBottom(this.xScale(dateRange))
+      .ticks(d3.timeDay.every(1))
+      .tickFormat(d3.timeFormat('%b %d'));
 
-barBuild.drawAxis = function (svg, dateRange, max_domain) {
-  // create axis
-  let xAxis = d3
-    .axisBottom(this.xScale(dateRange))
-    .ticks(d3.timeDay.every(1))
-    .tickFormat(d3.timeFormat('%b %d'));
+    let yAxis = d3
+      .axisRight(this.yScale(d3.max([max_domain, 100])))
+      // .ticks(10)
+      .tickSize(this.width());
 
-  let yAxis = d3
-    .axisRight(this.yScale(d3.max([max_domain, 100])))
-    // .ticks(10)
-    .tickSize(this.width());
+    const drawnX = svg.select('.xaxis').transition().duration(500).call(xAxis);
 
-  const drawnX = svg.select('.xaxis').transition().duration(500).call(xAxis);
+    drawnX
+      .selectAll('text')
+      .style('text-anchor', 'end')
+      .attr('dx', '-.8em')
+      .attr('dy', '-.55em')
+      .attr('transform', 'rotate(-90)');
 
-  drawnX
-    .selectAll('text')
-    .style('text-anchor', 'end')
-    .attr('dx', '-.8em')
-    .attr('dy', '-.55em')
-    .attr('transform', 'rotate(-90)');
+    const drawnY = svg.select('.yaxis').transition().duration(500).call(yAxis);
 
-  const drawnY = svg.select('.yaxis').transition().duration(500).call(yAxis);
+    drawnY
+      .selectAll('.tick:not(:first-of-type) line')
+      .attr('stroke', '#777')
+      .attr('stroke-dasharray', '2,5')
+      .attr('transform', `translate(-${this.margin().left},0)`);
 
-  drawnY
-    .selectAll('.tick:not(:first-of-type) line')
-    .attr('stroke', '#777')
-    .attr('stroke-dasharray', '2,5')
-    .attr('transform', `translate(-${this.margin().left},0)`);
+    drawnY
+      .selectAll('.tick text')
+      .attr('x', -this.margin().left)
+      .attr('dy', -4);
 
-  drawnY.selectAll('.tick text').attr('x', -this.margin().left).attr('dy', -4);
+    drawnY.select('path').attr('stroke-width', '0');
+  },
+  drawBar: function ({
+    selector,
+    dateRange,
+    data,
+    dataStacked,
+    transactionsMap,
+    max_domain,
+    tooltip
+  }) {
+    if (data.length === 0) return;
+    // 100% of shift is the space between ticks
+    const widthsIncome = { bar: 0.4 * this.shift(), translate: 0 };
+    const widthsExpense = {
+      bar: 0.4 * this.shift(),
+      translate: -0.4 * this.shift()
+    };
 
-  drawnY.select('path').attr('stroke-width', '0');
-};
+    let colors = function (d) {
+      switch (d.transaction.type) {
+        case 'income':
+          return ['#a1d99b', '#41ab5d'];
+        case 'expense':
+          return ['#fb6a4a', '#cb181d'];
+        case 'transfer':
+          return ['#8029aa', '#d238f9'];
+        default:
+          return ['#0019ff'];
+      }
+    };
 
-barBuild.drawBar = function ({
-  selector,
-  dateRange,
-  data,
-  dataStacked,
-  transactionsMap,
-  max_domain,
-  tooltip
-}) {
-  if (data.length === 0) return;
-  // 100% of shift is the space between ticks
-  const widthsIncome = { bar: 0.4 * this.shift(), translate: 0 };
-  const widthsExpense = {
-    bar: 0.4 * this.shift(),
-    translate: -0.4 * this.shift()
-  };
+    // maybe adjust this scale?
+    let color = (d) =>
+      d3.scaleLinear().domain([0, data.length]).range(colors(d));
 
-  let colors = function (d) {
-    switch (d.transaction.type) {
-      case 'income':
-        return ['#a1d99b', '#41ab5d'];
-      case 'expense':
-        return ['#fb6a4a', '#cb181d'];
-      case 'transfer':
-        return ['#8029aa', '#d238f9'];
-      default:
-        return ['#0019ff'];
+    const groups = selector
+      .selectAll('g')
+      .filter((d, i) => `#${i}-${d.id}`)
+      .data(data)
+      .join(
+        (enter) => enter.insert('g'),
+        (update) => update,
+        (exit) => exit.remove()
+      );
+
+    const rectGroups = groups
+      .join(
+        (enter) => enter.insert('g'),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .attr('class', (d) => d.transaction.type)
+      .attr('id', (d, i) => `${i}-${d.id}`)
+      .style('fill', (d, i) => color(d)(i))
+      .attr(
+        'transform',
+        (d) =>
+          `translate(${
+            (d.transaction.type === 'income' ? widthsIncome : widthsExpense)
+              .translate
+          },${0})`
+      )
+      .on('mouseenter', function () {
+        tooltip.target.transition().duration(200).style('opacity', 0.9);
+      })
+      .on('mouseover', function (event, d) {
+        tooltip.render(
+          { pageX: event.pageX, pageY: event.pageY },
+          d,
+          tooltip.target
+        );
+      })
+      .on('mouseout', function () {
+        tooltip.target.transition().duration(500).style('opacity', 0);
+      });
+
+    let xScale = barBuild.xScale(dateRange);
+    let yScale = barBuild.yScale(max_domain);
+    let rects = groups
+      .selectAll(`rect`)
+      .data((d) => d.stacked.filter((d) => d.height !== 0))
+      .join(
+        (enter) => enter.append('rect'),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .attr('x', (d) => xScale(d.date))
+      .attr('y', (d) => yScale(0))
+      .attr('rx', 3)
+      .attr('height', (d) => 0)
+      .attr('width', (d, i, nodes) => {
+        const dataType = nodes[i].parentNode.__data__.transaction.type;
+        return (dataType === 'income' ? widthsIncome : widthsExpense).bar;
+      })
+      .transition()
+      .duration(1500)
+      .ease(d3.easeBounceOut)
+      .attr('y', (d) => yScale(d.height + d.y0))
+      .attr(
+        'height',
+        (d) =>
+          // as the scale is inversed, flip
+          this.height() -
+          this.margin().top -
+          this.margin().bottom -
+          yScale(d.height)
+      );
+  },
+  drawLine: function ({ svg, selector, dateRange, data, max_domain, tooltip }) {
+    if (!data || data.length === 0) {
+      selector.selectAll('path').data(data).exit().remove();
+      return;
     }
-  };
+    let linecolors = d3.scaleOrdinal(d3.schemeCategory10);
+    let marginLeft = this.margin().left;
 
-  // maybe adjust this scale?
-  let color = (d) => d3.scaleLinear().domain([0, data.length]).range(colors(d));
+    const dates = eachDayOfInterval(dateRange);
+    const xScale = barBuild.xScale(dateRange);
+    const yScale = barBuild.yScale(max_domain);
 
-  const groups = selector
-    .selectAll('g')
-    .filter((d, i) => `#${i}-${d.id}`)
-    .data(data)
-    .join(
-      (enter) => enter.insert('g'),
-      (update) => update,
-      (exit) => exit.remove()
-    );
+    const line = d3
+      .line()
+      .x((d) => xScale(d[0]))
+      .y((d) => yScale(d[1]));
 
-  const rectGroups = groups
-    .join(
-      (enter) => enter.insert('g'),
-      (update) => update,
-      (exit) => exit.remove()
-    )
-    .attr('class', (d) => d.transaction.type)
-    .attr('id', (d, i) => `${i}-${d.id}`)
-    .style('fill', (d, i) => color(d)(i))
-    .attr(
-      'transform',
-      (d) =>
-        `translate(${
-          (d.transaction.type === 'income' ? widthsIncome : widthsExpense)
-            .translate
-        },${0})`
-    )
-    .on('mouseenter', function () {
-      tooltip.target.transition().duration(200).style('opacity', 0.9);
-    })
-    .on('mouseover', function (event, d) {
-      tooltip.render(
-        { pageX: event.pageX, pageY: event.pageY },
-        d,
-        tooltip.target
-      );
-    })
-    .on('mouseout', function () {
-      tooltip.target.transition().duration(500).style('opacity', 0);
-    });
+    selector
+      .selectAll('path')
+      .data(data)
+      .join(
+        (enter) => enter.append('path'),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .attr('d', (d) => line(d.data))
+      .attr('stroke', (d, i) => linecolors(i))
+      .attr('stroke-width', 2)
+      .attr('fill', 'none');
 
-  let xScale = barBuild.xScale(dateRange);
-  let yScale = barBuild.yScale(max_domain);
-  let rects = groups
-    .selectAll(`rect`)
-    .data((d) => d.stacked.filter((d) => d.height !== 0))
-    .join(
-      (enter) => enter.append('rect'),
-      (update) => update,
-      (exit) => exit.remove()
-    )
-    .attr('x', (d) => xScale(d.date))
-    .attr('y', (d) => yScale(0))
-    .attr('rx', 3)
-    .attr('height', (d) => 0)
-    .attr('width', (d, i, nodes) => {
-      const dataType = nodes[i].parentNode.__data__.transaction.type;
-      return (dataType === 'income' ? widthsIncome : widthsExpense).bar;
-    })
-    .transition()
-    .duration(1500)
-    .ease(d3.easeBounceOut)
-    .attr('y', (d) => yScale(d.height + d.y0))
-    .attr(
-      'height',
-      (d) =>
-        // as the scale is inversed, flip
-        this.height() -
-        this.margin().top -
-        this.margin().bottom -
-        yScale(d.height)
-    );
-};
+    svg
+      .on('mouseenter', function () {
+        tooltip.target.transition().duration(200).style('opacity', 0.9);
+        tooltip.line.transition().duration(200).style('opacity', 0.9);
+      })
+      .on('mousemove', function (event) {
+        let positionX = event.offsetX;
+        // cause we have weird translations right now
+        const dateExact = xScale.invert(positionX - (60 * 2) / 3);
+        const dateIndex = closestIndexTo(dateExact, dates);
+        const date = dates[dateIndex];
+        const tooltipLinePositionX = xScale(date);
 
-barBuild.drawLine = function ({
-  svg,
-  selector,
-  dateRange,
-  data,
-  max_domain,
-  tooltip
-}) {
-  if (!data || data.length === 0) {
-    selector.selectAll('path').data(data).exit().remove();
-    return;
+        tooltip.line
+          .transition()
+          .duration(400)
+          .ease(d3.easeBackOut)
+          .attr('x1', tooltipLinePositionX)
+          .attr('x2', tooltipLinePositionX)
+          .attr('y1', 0)
+          .attr('y2', max_domain)
+          .style('opacity', 0.9);
+
+        const lineValues = data.map((d) => ({
+          ...d,
+          valueStart: d.data[dateIndex * 2][1],
+          valueEnd: d.data[dateIndex * 2 + 1][1]
+        }));
+        tooltip.render(
+          { pageX: event.pageX, pageY: event.pageY },
+          lineValues,
+          tooltip.target
+        );
+      })
+      .on('mouseout', function () {
+        tooltip.target.transition().duration(500).style('opacity', 0);
+        tooltip.line.transition().duration(500).style('opacity', 0);
+      });
   }
-  let linecolors = d3.scaleOrdinal(d3.schemeCategory10);
-  let marginLeft = this.margin().left;
-
-  const dates = eachDayOfInterval(dateRange);
-  const xScale = barBuild.xScale(dateRange);
-  const yScale = barBuild.yScale(max_domain);
-
-  const line = d3
-    .line()
-    .x((d) => xScale(d[0]))
-    .y((d) => yScale(d[1]));
-
-  selector
-    .selectAll('path')
-    .data(data)
-    .join(
-      (enter) => enter.append('path'),
-      (update) => update,
-      (exit) => exit.remove()
-    )
-    .attr('d', (d) => line(d.data))
-    .attr('stroke', (d, i) => linecolors(i))
-    .attr('stroke-width', 2)
-    .attr('fill', 'none');
-
-  svg
-    .on('mouseenter', function () {
-      tooltip.target.transition().duration(200).style('opacity', 0.9);
-      tooltip.line.transition().duration(200).style('opacity', 0.9);
-    })
-    .on('mousemove', function (event) {
-      let positionX = event.offsetX;
-      // cause we have weird translations right now
-      const dateExact = xScale.invert(positionX - (60 * 2) / 3);
-      const dateIndex = closestIndexTo(dateExact, dates);
-      const date = dates[dateIndex];
-      const tooltipLinePositionX = xScale(date);
-
-      tooltip.line
-        .transition()
-        .duration(400)
-        .ease(d3.easeBackOut)
-        .attr('x1', tooltipLinePositionX)
-        .attr('x2', tooltipLinePositionX)
-        .attr('y1', 0)
-        .attr('y2', max_domain)
-        .style('opacity', 0.9);
-
-      const lineValues = data.map((d) => ({
-        ...d,
-        valueStart: d.data[dateIndex * 2][1],
-        valueEnd: d.data[dateIndex * 2 + 1][1]
-      }));
-      tooltip.render(
-        { pageX: event.pageX, pageY: event.pageY },
-        lineValues,
-        tooltip.target
-      );
-    })
-    .on('mouseout', function () {
-      tooltip.target.transition().duration(500).style('opacity', 0);
-      tooltip.line.transition().duration(500).style('opacity', 0);
-    });
 };
--- src/pages/flow/index.tsx
+++ src/pages/flow/index.tsx
@@ -1,7 +1,11 @@
 import { parseDate } from '@internationalized/date';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'starfx/react';
 
+import {
+  ChartAccounts,
+  lineChartAccounts
+} from '~/src/store/selectors/accounts';
 import { dateRangeWithStrings } from '~/src/store/selectors/chartRange.ts';
 import { updateChartDateRange } from '~/src/store/thunks/chartRange.ts';
 
@@ -12,20 +16,52 @@ import BarChart from './barChart.tsx';
 const FinancialFlow = () => {
   const dispatch = useDispatch();
   const dateRange = useSelector(dateRangeWithStrings);
+  const [vehicleFilter, setVehicleFilter] = useState('all');
+  const accountData = useSelector(lineChartAccounts);
+  const [accountFilters, setAccountFilters] = useState<string[]>([]);
+  const accounts =
+    vehicleFilter === 'all'
+      ? accountData.data
+      : accountData.data.filter((d) => d.vehicle === vehicleFilter);
+
   return (
-    <div className="container mx-auto my-2 px-4 sm:px-6 lg:px-8">
+    <div className="container mx-auto my-2 px-0 sm:px-4 lg:px-8">
       <h1 className="text-3xl font-semibold py-3">Cash Flow</h1>
+      <div className="overflow-auto px-4 py-4 sm:px-6 flex items-center space-x-8">
+        <DatePicker
+          label="Starting Date"
+          value={parseDate(dateRange.startString)}
+          onChange={(calendar) =>
+            dispatch(updateChartDateRange(calendar.toString()))
+          }
+        />
+        <DataFilterSelector
+          vehicleFilter={vehicleFilter}
+          setVehicleFilter={setVehicleFilter}
+          setAccountFilters={setAccountFilters}
+          vehicles={[
+            'all',
+            'operating',
+            'investment',
+            'debt',
+            'loan',
+            'credit line'
+          ]}
+        />
+      </div>
+      <AccountListFilter
+        accountFilters={accountFilters}
+        setAccountFilters={setAccountFilters}
+        accounts={accounts}
+      />
       <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
         <div className="px-4 py-5 sm:p-6">
-          <BarChart dateRange={dateRange} />
-        </div>
-        <div className="px-4 py-4 sm:px-6">
-          <DatePicker
-            label="Starting Date"
-            value={parseDate(dateRange.startString)}
-            onChange={(calendar) =>
-              dispatch(updateChartDateRange(calendar.toString()))
-            }
+          <BarChart
+            dateRange={dateRange}
+            accounts={accounts.filter(
+              (account) => !accountFilters.includes(account.name)
+            )}
+            vehicleFilter={vehicleFilter}
           />
         </div>
       </div>
@@ -34,3 +70,79 @@ const FinancialFlow = () => {
 };
 
 export default FinancialFlow;
+
+const DataFilterSelector = ({
+  vehicles,
+  vehicleFilter,
+  setVehicleFilter,
+  setAccountFilters
+}: {
+  vehicles: string[];
+  vehicleFilter: string;
+  setVehicleFilter: React.Dispatch<React.SetStateAction<string>>;
+  setAccountFilters: React.Dispatch<React.SetStateAction<string[]>>;
+}) => {
+  return (
+    <fieldset className="mt-6">
+      <legend className="block text-sm font-semibold text-gray-900">
+        Choose An Account Vehicle
+      </legend>
+      <div className="flex items-center space-x-3">
+        {vehicles.map((vehicle) => (
+          <div key={vehicle} className="flex items-center">
+            <input
+              id={vehicle}
+              name="account-filter"
+              type="radio"
+              className="h-4 w-4 border-gray-300 text-cyan-700 focus:ring-cyan-600"
+              value={vehicle}
+              checked={vehicleFilter === vehicle}
+              onChange={(event) => {
+                setAccountFilters([]);
+                setVehicleFilter(event.target.value);
+              }}
+            />
+            <label
+              htmlFor={vehicle}
+              className="ml-3 block text-sm font-medium leading-6 text-gray-900"
+            >
+              {vehicle}
+            </label>
+          </div>
+        ))}
+      </div>
+    </fieldset>
+  );
+};
+
+const AccountListFilter = ({
+  accounts,
+  accountFilters,
+  setAccountFilters
+}: {
+  accounts: ChartAccounts['data'];
+  accountFilters: string[];
+  setAccountFilters: React.Dispatch<React.SetStateAction<string[]>>;
+}) => {
+  return accounts.map((account) => (
+    <span
+      className={`mx-1 inline-flex items-center gap-x-1.5 rounded-full px-4 py-1 text-xs font-medium text-gray-600${accountFilters.includes(account.name) ? '' : ' bg-gray-100'}`}
+      onClick={() =>
+        setAccountFilters((state) =>
+          state.includes(account.name)
+            ? state.filter((a) => a !== account.name)
+            : state.concat(account.name)
+        )
+      }
+    >
+      <svg
+        className="h-1.5 w-1.5 fill-gray-400"
+        viewBox="0 0 6 6"
+        aria-hidden="true"
+      >
+        <circle cx="3" cy="3" r="3" />
+      </svg>
+      {account.name}
+    </span>
+  ));
+};
