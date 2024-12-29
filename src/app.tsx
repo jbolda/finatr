7f557fd77279444e2fe50887dd58b7ddf1773df8
Jacob Bolda <me@jacobbolda.com>
2024-12-29 14:39:02 -0600

    sidebar nav to handle more pages

--- src/app.tsx
+++ src/app.tsx
@@ -3,16 +3,16 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { type SupabaseClient } from '@supabase/supabase-js';
 import React from 'react';
 import { RouterProvider } from 'react-aria-components';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes as RoutesList, Route, Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'starfx/react';
 
 import { Footer } from './components/Footer.tsx';
-import { Header } from './components/Header.tsx';
+import Sidebar from './components/Sidebar.tsx';
 import Examples from './pages/examples';
 import Homepage from './pages/homepage';
 import TransactionsOverview from './pages/transactions/index.tsx';
-import { schema } from './store/schema.ts';
+import { schema, type Settings as SettingsStore } from './store/schema.ts';
 
 const Settings = React.lazy(() => import('./pages/settings'));
 const Financial = React.lazy(() => import('./pages/flow'));
@@ -34,158 +34,172 @@ const FinancialIndependence = React.lazy(
 const Importing = React.lazy(() => import('./pages/importing'));
 const Taxes = React.lazy(() => import('./pages/taxes'));
 
-const FeatureFlag = ({ flag, children }) => {
+function FeatureFlag({
+  flag,
+  children
+}: {
+  flag: boolean;
+  children: React.ReactNode;
+}) {
   if (flag) return children;
   return <NoMatch />;
-};
+}
 
-const App = ({
+function AppWrapper({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+
+  return <RouterProvider navigate={navigate}>{children} </RouterProvider>;
+}
+
+function App({
   supabase
 }: {
   supabase: SupabaseClient<any, 'public', any> | null;
-}) => {
+}) {
   const settings = useSelector(schema.settings.select);
-  const auth = useSelector(schema.auth.select);
-  let navigate = useNavigate();
 
   return (
-    <RouterProvider navigate={navigate}>
-      <div className="min-h-full">
-        <div className="flex flex-col h-screen">
-          <Header settings={settings} />
+    <AppWrapper>
+      <Sidebar />
 
-          <main className="flex-grow -mt-32">
-            <div className="mx-auto container pb-12">
-              <div className="rounded-lg bg-white px-1 py-6 shadow sm:px-5">
-                <Routes>
-                  <Route index element={<Homepage />} />
-                  <Route path="examples" element={<Examples />} />
-                  <Route
-                    path="auth"
-                    element={
-                      supabase && !auth.user ? (
-                        <Auth
-                          supabaseClient={supabase}
-                          appearance={{ theme: ThemeSupa }}
-                        />
-                      ) : (
-                        <div>Logged in!</div>
-                      )
-                    }
-                  />
-                  <Route
-                    path="settings"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <Settings />
-                      </React.Suspense>
-                    }
-                  />
-                  <Route
-                    path="flow"
-                    element={
-                      <FeatureFlag flag={settings.flow}>
-                        <React.Suspense fallback={<>...</>}>
-                          <Financial />
-                        </React.Suspense>
-                      </FeatureFlag>
-                    }
-                  />
-                  <Route
-                    path="accounts"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <AccountsContainer />
-                      </React.Suspense>
-                    }
-                  >
-                    <Route
-                      index
-                      element={
-                        <React.Suspense fallback={<>...</>}>
-                          <Accounts />
-                        </React.Suspense>
-                      }
-                    />
-                    <Route
-                      path="set"
-                      element={
-                        <React.Suspense fallback={<>...</>}>
-                          <AccountInput />
-                        </React.Suspense>
-                      }
-                    />
-                  </Route>
-                  <Route
-                    path="transactions"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <TransactionsContainer />
-                      </React.Suspense>
-                    }
-                  >
-                    <Route
-                      index
-                      element={
-                        <React.Suspense fallback={<>...</>}>
-                          <TransactionsOverview />
-                        </React.Suspense>
-                      }
-                    />
-                    <Route
-                      path="set"
-                      element={
-                        <React.Suspense fallback={<>...</>}>
-                          <TransactionInput />
-                        </React.Suspense>
-                      }
-                    />
-                  </Route>
-                  <Route
-                    path="planning"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <Planning />
-                      </React.Suspense>
-                    }
-                  />
-                  <Route
-                    path="financialindependence"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <FinancialIndependence />
-                      </React.Suspense>
-                    }
-                  />
-                  <Route
-                    path="import"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <Importing />
-                      </React.Suspense>
-                    }
-                  />
-                  <Route
-                    path="taxes"
-                    element={
-                      <React.Suspense fallback={<>...</>}>
-                        <Taxes />
-                      </React.Suspense>
-                    }
-                  />
-
-                  <Route path="*" element={<NoMatch />} />
-                </Routes>
-              </div>
-            </div>
-          </main>
-
-          <Footer settings={settings} />
+      <main className="py-10 lg:pl-56">
+        <div className="px-4 sm:px-6 lg:px-8">
+          <Routes supabase={supabase} settings={settings} />
         </div>
-      </div>
-    </RouterProvider>
+      </main>
+
+      <Footer settings={settings} />
+    </AppWrapper>
   );
-};
+}
+
+function Routes({
+  supabase,
+  settings
+}: {
+  supabase: SupabaseClient<any, 'public', any> | null;
+  settings: SettingsStore;
+}) {
+  const auth = useSelector(schema.auth.select);
+  return (
+    <RoutesList>
+      <Route index element={<Homepage />} />
+      <Route path="examples" element={<Examples />} />
+      <Route
+        path="auth"
+        element={
+          supabase && !auth.user ? (
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+          ) : (
+            <div>Logged in!</div>
+          )
+        }
+      />
+      <Route
+        path="settings"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <Settings />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="flow"
+        element={
+          <FeatureFlag flag={settings.flow}>
+            <React.Suspense fallback={<>...</>}>
+              <Financial />
+            </React.Suspense>
+          </FeatureFlag>
+        }
+      />
+      <Route
+        path="accounts"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <AccountsContainer />
+          </React.Suspense>
+        }
+      >
+        <Route
+          index
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Accounts />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="set"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <AccountInput />
+            </React.Suspense>
+          }
+        />
+      </Route>
+      <Route
+        path="transactions"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <TransactionsContainer />
+          </React.Suspense>
+        }
+      >
+        <Route
+          index
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <TransactionsOverview />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="set"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <TransactionInput />
+            </React.Suspense>
+          }
+        />
+      </Route>
+      <Route
+        path="planning"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <Planning />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="financialindependence"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <FinancialIndependence />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="import"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <Importing />
+          </React.Suspense>
+        }
+      />
+      <Route
+        path="taxes"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <Taxes />
+          </React.Suspense>
+        }
+      />
+
+      <Route path="*" element={<NoMatch />} />
+    </RoutesList>
+  );
+}
 
 function NoMatch() {
   return (
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,255 @@
+import {
+  ArrowRightLeft,
+  Banknote,
+  ChartColumnIncreasing,
+  HandCoins,
+  Landmark,
+  Menu as MenuIcon,
+  Presentation,
+  SettingsIcon,
+  TrendingUp
+} from 'lucide-react';
+import React from 'react';
+import {
+  Link,
+  Menu,
+  Button,
+  MenuItem,
+  MenuTrigger,
+  Popover,
+  MenuSection,
+  Header,
+  Collection
+} from 'react-aria-components';
+import { useSelector } from 'starfx/react';
+import { tv } from 'tailwind-variants';
+
+import { schema } from '../store/schema';
+
+type NavItem = {
+  name: string;
+  to: string;
+  icon: typeof Presentation;
+  tag?: string;
+};
+
+const navigation: NavItem[] = [
+  { name: 'Examples', to: '/examples', icon: Presentation },
+  { name: 'Import/Export', to: '/import', icon: ArrowRightLeft },
+  { name: 'Accounts', to: '/accounts', icon: Landmark },
+  { name: 'Transactions', to: '/transactions', icon: HandCoins },
+  { name: 'FI', to: '/financialindependence', icon: TrendingUp },
+  { name: 'Taxes', tag: 'alpha', to: '/taxes', icon: Banknote }
+];
+
+const graphs: NavItem[] = [
+  { name: 'Planning', to: '/planning', icon: MenuIcon },
+  { name: 'Cash Flow', to: '/flow', icon: MenuIcon }
+];
+
+let fullNavigation = [
+  {
+    name: 'Records',
+    id: 'records',
+    children: navigation
+  },
+  {
+    name: 'Graphs',
+    id: 'graphs',
+    children: graphs
+  }
+];
+
+/*
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-white">
+  <body class="h-full">
+  ```
+*/
+export default function Sidebar() {
+  const settings = useSelector(schema.settings.select);
+
+  return (
+    <>
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-56 lg:flex-col">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+          <div className="flex h-16 shrink-0 items-center">
+            <Link href="/">
+              <span className="sr-only">Home</span>
+              <ChartColumnIncreasing color="white" className="h-8 w-auto" />
+            </Link>
+            <div className="flex-1 text-sm/6 font-semibold text-white pl-3">
+              Finatr
+            </div>
+          </div>
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <Menu
+                  className="-mx-2 space-y-1"
+                  selectionMode="single"
+                  aria-label="navigation"
+                  items={navigation.filter(
+                    (item) => settings[item.to.substring(1)] !== false
+                  )}
+                  selectedKeys={[location.pathname]}
+                >
+                  {(item) => (
+                    <MenuItem
+                      id={item.to}
+                      href={item.to}
+                      aria-label={item.name}
+                      className={() =>
+                        sidebarItem({
+                          link:
+                            location.pathname === item.to
+                              ? 'selected'
+                              : 'default'
+                        })
+                      }
+                    >
+                      <item.icon
+                        aria-hidden="true"
+                        className="size-6 shrink-0"
+                      />
+                      {item.name}
+                      {item?.tag ? <sup>{item.tag}</sup> : null}
+                    </MenuItem>
+                  )}
+                </Menu>
+              </li>
+              <li>
+                <div className="text-xs/6 font-semibold text-gray-400">
+                  Graphs
+                </div>
+                <Menu
+                  className="-mx-2 mt-2 space-y-1"
+                  selectionMode="single"
+                  aria-label="graphs"
+                  items={graphs.filter(
+                    (item) => settings[item.to.substring(1)] !== false
+                  )}
+                  selectedKeys={[location.pathname]}
+                >
+                  {(graph) => (
+                    <MenuItem
+                      id={graph.to}
+                      href={graph.to}
+                      aria-label={graph.name}
+                      className={() =>
+                        sidebarItem({
+                          link:
+                            location.pathname === graph.to
+                              ? 'selected'
+                              : 'default'
+                        })
+                      }
+                    >
+                      <span className="flex size-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
+                        {graph.name.substring(0, 1)}
+                      </span>
+                      <span className="truncate">{graph.name}</span>
+                    </MenuItem>
+                  )}
+                </Menu>
+              </li>
+              <li className="-mx-6 mt-auto">
+                <Link
+                  href="/settings"
+                  className={() =>
+                    `flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white ${location.pathname === '/settings' ? 'bg-gray-800' : 'hover:bg-gray-800'}`
+                  }
+                >
+                  <SettingsIcon />
+                  <span>Settings</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      {/* Mobile menu */}
+      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+        <MenuTrigger>
+          <Button
+            aria-label="Menu"
+            className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <MenuIcon className="size-6" aria-hidden="true" />
+          </Button>
+          <Popover className="relative z-50 lg:hidden">
+            {/* visual shading */}
+            <div className="pointer-events-none fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0" />
+            {/* sidebar */}
+            <div className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full">
+              <div className="flex grow flex-col gap-y-5 rounded-md overflow-y-auto bg-gray-900 px-6 p-2 ring-1 ring-white/10">
+                <Menu
+                  className="outline-none"
+                  aria-label="navigation popover"
+                  selectionMode="single"
+                  items={fullNavigation}
+                  selectedKeys={[location.pathname]}
+                >
+                  {(section) => (
+                    <MenuSection>
+                      <Header className="text-xs/6 font-bold text-gray-400 mt-3">
+                        {section.name}
+                      </Header>
+                      <Collection items={section.children}>
+                        {(item) => (
+                          <MenuItem
+                            id={item.to}
+                            href={item.to}
+                            aria-label={item.name}
+                            className={() =>
+                              sidebarItem({
+                                link:
+                                  location.pathname === item.to
+                                    ? 'selected'
+                                    : 'default'
+                              })
+                            }
+                          >
+                            <span className="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium border-transparent">
+                              {item.name}{' '}
+                              {item?.tag ? <sup>{item.tag}</sup> : null}
+                            </span>
+                          </MenuItem>
+                        )}
+                      </Collection>
+                    </MenuSection>
+                  )}
+                </Menu>
+              </div>
+            </div>
+          </Popover>
+        </MenuTrigger>
+
+        <div className="flex-1 text-sm/6 font-semibold text-white">Finatr</div>
+        <Link
+          href="/settings"
+          className={() =>
+            `flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white ${location.pathname === '/settings' ? 'bg-gray-800' : 'hover:bg-gray-800'}`
+          }
+        >
+          <SettingsIcon />
+        </Link>
+      </div>
+    </>
+  );
+}
+
+const sidebarItem = tv({
+  base: 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+  variants: {
+    link: {
+      default: 'text-gray-400 hover:bg-gray-800 hover:text-white',
+      selected: 'bg-gray-800 text-white'
+    }
+  }
+});
