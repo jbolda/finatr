7d335d66819561cb38f5382d27455b7ed7e3d7d3
Jacob Bolda <me@jacobbolda.com>
2025-01-02 00:59:54 -0600

    use same menu content for sidebar and hamburger

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -59,135 +59,7 @@ let fullNavigation = [
     children: graphs
   }
 ];
-
-/*
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
-*/
-export default function Sidebar() {
-  const settings = useSelector(schema.settings.select);
-
-  return (
-    <>
-      {/* Static sidebar for desktop */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-56 lg:flex-col">
-        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-          <div className="flex h-16 shrink-0 items-center">
-            <Link href="/">
-              <span className="sr-only">Home</span>
-              <ChartColumnIncreasing color="white" className="h-8 w-auto" />
-            </Link>
-            <div className="flex-1 text-sm/6 font-semibold text-white pl-3">
-              Finatr
-            </div>
-          </div>
-          <nav className="flex flex-1 flex-col">
-            <ul role="list" className="flex flex-1 flex-col gap-y-7">
-              <li>
-                <Menu
-                  className="-mx-2 space-y-1"
-                  selectionMode="single"
-                  aria-label="navigation"
-                  items={navigation.filter(
-                    (item) => settings[item.to.substring(1)] !== false
-                  )}
-                  selectedKeys={[location.pathname]}
-                >
-                  {(item) => (
-                    <MenuItem
-                      id={item.to}
-                      href={item.to}
-                      aria-label={item.name}
-                      className={() =>
-                        sidebarItem({
-                          link:
-                            location.pathname === item.to
-                              ? 'selected'
-                              : 'default'
-                        })
-                      }
-                    >
-                      <item.icon
-                        aria-hidden="true"
-                        className="size-6 shrink-0"
-                      />
-                      {item.name}
-                      {item?.tag ? <sup>{item.tag}</sup> : null}
-                    </MenuItem>
-                  )}
-                </Menu>
-              </li>
-              <li>
-                <div className="text-xs/6 font-semibold text-gray-400">
-                  Graphs
-                </div>
-                <Menu
-                  className="-mx-2 mt-2 space-y-1"
-                  selectionMode="single"
-                  aria-label="graphs"
-                  items={graphs.filter(
-                    (item) => settings[item.to.substring(1)] !== false
-                  )}
-                  selectedKeys={[location.pathname]}
-                >
-                  {(graph) => (
-                    <MenuItem
-                      id={graph.to}
-                      href={graph.to}
-                      aria-label={graph.name}
-                      className={() =>
-                        sidebarItem({
-                          link:
-                            location.pathname === graph.to
-                              ? 'selected'
-                              : 'default'
-                        })
-                      }
-                    >
-                      <span className="flex size-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                        {graph.name.substring(0, 1)}
-                      </span>
-                      <span className="truncate">{graph.name}</span>
-                    </MenuItem>
-                  )}
-                </Menu>
-              </li>
-              <li className="-mx-6 mt-auto">
-                <Link
-                  href="/settings"
-                  className={() =>
-                    `flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white ${location.pathname === '/settings' ? 'bg-gray-800' : 'hover:bg-gray-800'}`
-                  }
-                >
-                  <SettingsIcon />
-                  <span>Settings</span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-
-      {/* Mobile menu */}
-      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-        <MenuTrigger>
-          <Button
-            aria-label="Menu"
-            className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
-          >
-            <span className="sr-only">Open sidebar</span>
-            <MenuIcon className="size-6" aria-hidden="true" />
-          </Button>
-          <Popover className="relative z-50 lg:hidden">
-            {/* visual shading */}
-            <div className="pointer-events-none fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0" />
-            {/* sidebar */}
-            <div className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full">
-              <div className="flex grow flex-col gap-y-5 rounded-md overflow-y-auto bg-gray-900 px-6 p-2 ring-1 ring-white/10">
+/* for using fullNavatiion, swap to this
                 <Menu
                   className="outline-none"
                   aria-label="navigation popover"
@@ -224,13 +96,49 @@ export default function Sidebar() {
                       </Collection>
                     </MenuSection>
                   )}
-                </Menu>
-              </div>
-            </div>
+                </Menu> */
+
+/*
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-white">
+  <body class="h-full">
+  ```
+*/
+export default function Sidebar() {
+  const settings = useSelector(schema.settings.select);
+
+  return (
+    <>
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-56 lg:flex-col">
+        <SidebarContent withHeader={true} />
+      </div>
+
+      {/* Mobile menu */}
+      <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+        <MenuTrigger>
+          <Button
+            aria-label="Menu"
+            className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <MenuIcon className="size-6" aria-hidden="true" />
+          </Button>
+          <Popover
+            className="relative z-50 lg:hidden"
+            crossOffset={-100}
+            containerPadding={0}
+          >
+            {/* visual shading */}
+            <div className="pointer-events-none fixed -z-40 inset-0 bg-gray-900/80 transition-opacity duration-3000 ease-linear data-[closed]:opacity-0" />
+            <SidebarContent withHeader={false} />
           </Popover>
         </MenuTrigger>
-
-        <div className="flex-1 text-sm/6 font-semibold text-white">Finatr</div>
+        <Link href="/" className="flex-1 text-sm/6 font-semibold text-white">
+          Finatr
+        </Link>
         <Link
           href="/settings"
           className={() =>
@@ -241,6 +149,100 @@ export default function Sidebar() {
         </Link>
       </div>
     </>
+  );
+}
+
+function SidebarContent({ withHeader = true }: { withHeader: boolean }) {
+  const settings = useSelector(schema.settings.select);
+  return (
+    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+      {withHeader ? (
+        <div className="flex h-16 shrink-0 items-center">
+          <Link href="/">
+            <span className="sr-only">Home</span>
+            <ChartColumnIncreasing color="white" className="h-8 w-auto" />
+          </Link>
+          <div className="flex-1 text-sm/6 font-semibold text-white pl-3">
+            Finatr
+          </div>
+        </div>
+      ) : null}
+      <nav className="flex flex-1 flex-col py-1">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <Menu
+              className="-mx-2 space-y-1"
+              selectionMode="single"
+              aria-label="navigation"
+              items={navigation.filter(
+                (item) => settings[item.to.substring(1)] !== false
+              )}
+              selectedKeys={[location.pathname]}
+            >
+              {(item) => (
+                <MenuItem
+                  id={item.to}
+                  href={item.to}
+                  aria-label={item.name}
+                  className={() =>
+                    sidebarItem({
+                      link:
+                        location.pathname === item.to ? 'selected' : 'default'
+                    })
+                  }
+                >
+                  <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                  {item.name}
+                  {item?.tag ? <sup>{item.tag}</sup> : null}
+                </MenuItem>
+              )}
+            </Menu>
+          </li>
+          <li>
+            <div className="text-xs/6 font-semibold text-gray-400">Graphs</div>
+            <Menu
+              className="-mx-2 mt-2 space-y-1"
+              selectionMode="single"
+              aria-label="graphs"
+              items={graphs.filter(
+                (item) => settings[item.to.substring(1)] !== false
+              )}
+              selectedKeys={[location.pathname]}
+            >
+              {(graph) => (
+                <MenuItem
+                  id={graph.to}
+                  href={graph.to}
+                  aria-label={graph.name}
+                  className={() =>
+                    sidebarItem({
+                      link:
+                        location.pathname === graph.to ? 'selected' : 'default'
+                    })
+                  }
+                >
+                  <span className="flex size-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
+                    {graph.name.substring(0, 1)}
+                  </span>
+                  <span className="truncate">{graph.name}</span>
+                </MenuItem>
+              )}
+            </Menu>
+          </li>
+          <li className="-mx-6 mt-auto">
+            <Link
+              href="/settings"
+              className={() =>
+                `flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-white ${location.pathname === '/settings' ? 'bg-gray-800' : 'hover:bg-gray-800'}`
+              }
+            >
+              <SettingsIcon />
+              <span>Settings</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
