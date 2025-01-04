37496a0bc11c77d159dd98a48d3b08439f926ce8
Jacob Bolda <me@jacobbolda.com>
2025-01-03 21:27:13 -0600

    collapsing sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
-import { set } from 'date-fns';
 import {
+  ArrowLeftToLine,
   ArrowRightLeft,
+  ArrowRightToLine,
   Banknote,
   ChartColumnIncreasing,
   ChartColumnStacked,
@@ -50,6 +51,13 @@ const graphs: NavItem[] = [
   { name: 'Cash Flow', to: '/flow', icon: ChartColumnStacked }
 ];
 
+const sidebarCollapse = tv({
+  base: 'hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:flex-col',
+  variants: {
+    sidebar: { wide: 'lg:w-56', collapsed: 'lg:w-16' }
+  }
+});
+
 /*
   This requires the follow classes in index.html
 
@@ -59,11 +67,18 @@ const graphs: NavItem[] = [
   ```
 */
 export default function Sidebar() {
+  const [sidebarState, setSidebarState] = React.useState<'wide' | 'collapsed'>(
+    'wide'
+  );
   return (
     <>
       {/* Static sidebar for desktop */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-56 lg:flex-col">
-        <SidebarContent withHeader={true} />
+      <div className={sidebarCollapse({ sidebar: sidebarState })}>
+        <SidebarContent
+          withHeader={true}
+          sidebarWidth={sidebarState}
+          setSidebarState={setSidebarState}
+        />
       </div>
 
       {/* Mobile menu */}
@@ -83,7 +98,7 @@ export default function Sidebar() {
           >
             {/* visual shading */}
             <div className="pointer-events-none fixed -z-40 inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0" />
-            <SidebarContent withHeader={false} />
+            <SidebarContent withHeader={false} sidebarWidth="wide" />
           </Popover>
         </MenuTrigger>
         <Link href="/" className="flex-1 text-sm/6 font-semibold text-white">
@@ -102,7 +117,25 @@ export default function Sidebar() {
   );
 }
 
-function SidebarContent({ withHeader = true }: { withHeader: boolean }) {
+const sidebarItem = tv({
+  base: 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+  variants: {
+    link: {
+      default: 'text-gray-400 hover:bg-gray-800 hover:text-white',
+      selected: 'bg-gray-800 text-white'
+    }
+  }
+});
+
+function SidebarContent({
+  withHeader = true,
+  sidebarWidth = 'wide',
+  setSidebarState
+}: {
+  withHeader: boolean;
+  sidebarWidth?: 'wide' | 'collapsed';
+  setSidebarState?: React.Dispatch<React.SetStateAction<'wide' | 'collapsed'>>;
+}) {
   const settings = useSelector(schema.settings.select);
   const fullNavigation = [
     {
@@ -118,16 +151,36 @@ function SidebarContent({ withHeader = true }: { withHeader: boolean }) {
   ];
 
   return (
-    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+    <div className="flex grow flex-col gap-y-5 overflow-y-auto overflow-x-hidden bg-gray-900 pl-5">
       {withHeader ? (
         <div className="flex h-16 shrink-0 items-center">
-          <Link href="/">
-            <span className="sr-only">Home</span>
-            <ChartColumnIncreasing color="white" className="h-8 w-auto" />
-          </Link>
-          <div className="flex-1 text-sm/6 font-semibold text-white pl-3">
-            Finatr
-          </div>
+          {sidebarWidth === 'wide' ? (
+            <>
+              <Link href="/">
+                <span className="sr-only">Home</span>
+                <ChartColumnIncreasing color="white" className="h-8 w-auto" />
+              </Link>
+              <div className="flex-1 text-sm/6 font-semibold text-white pl-3">
+                Finatr
+              </div>
+            </>
+          ) : null}
+          {setSidebarState ? (
+            <Button
+              className="pr-1 text-white bg-gray-900"
+              onPress={() =>
+                setSidebarState((state) =>
+                  state === 'wide' ? 'collapsed' : 'wide'
+                )
+              }
+            >
+              {sidebarWidth === 'wide' ? (
+                <ArrowLeftToLine />
+              ) : (
+                <ArrowRightToLine />
+              )}
+            </Button>
+          ) : null}
         </div>
       ) : null}
       <nav className="flex flex-1 flex-col py-1">
@@ -137,6 +190,7 @@ function SidebarContent({ withHeader = true }: { withHeader: boolean }) {
           aria-label="navigation"
           items={fullNavigation ?? settings}
           selectedKeys={[location.pathname]}
+          dependencies={[sidebarWidth]}
         >
           {(section) => (
             <MenuSection>
@@ -159,8 +213,10 @@ function SidebarContent({ withHeader = true }: { withHeader: boolean }) {
                     }
                   >
                     <item.icon aria-hidden="true" className="size-6 shrink-0" />
-                    {item.name}
-                    {item?.tag ? <sup>{item.tag}</sup> : null}
+                    {sidebarWidth === 'wide' ? item.name : null}
+                    {sidebarWidth === 'wide' && item?.tag ? (
+                      <sup>{item.tag}</sup>
+                    ) : null}
                   </MenuItem>
                 )}
               </Collection>
@@ -176,20 +232,10 @@ function SidebarContent({ withHeader = true }: { withHeader: boolean }) {
             }
           >
             <SettingsIcon />
-            <span>Settings</span>
+            {sidebarWidth === 'wide' ? <span>Settings</span> : null}
           </Link>
         </div>
       </nav>
     </div>
   );
 }
-
-const sidebarItem = tv({
-  base: 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
-  variants: {
-    link: {
-      default: 'text-gray-400 hover:bg-gray-800 hover:text-white',
-      selected: 'bg-gray-800 text-white'
-    }
-  }
-});
