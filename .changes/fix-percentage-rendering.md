89364639814eaf6a18c49749f0b0772225527a10
Jacob Bolda <me@jacobbolda.com>
2025-02-05 00:21:57 -0600

    fix percentage render (#906)
    * fix percentage render
    
    * add tests

---
+++ .changes/fix-percentage-rendering.md
@@ -0,0 +1,5 @@
+---
+"web": patch
+---
+
+Fix rendering of percentages that we don't lose a decimal place and mis-render any percentages"
--- src/store/utils/dineroUtils.ts
+++ src/store/utils/dineroUtils.ts
@@ -90,20 +90,25 @@ export const toHumanInterest = ({
 }) => {
   if (amount === 0) return `${leadingSymbol}0${trailingSymbol}`;
   const stringifiedArray = amount.toString().split('');
+  // so we don't splice out for percentages
+  if (trailingSymbol === '%') stringifiedArray.push('0', '0');
   // apply operations backwards
   stringifiedArray.reverse();
-  stringifiedArray.splice(-scale - 2, 0, '.');
+  // percentage conversion happens with zero push above
+  stringifiedArray.splice(-scale, 0, '.');
 
   // remove trailing zeros
   let trailingZeros = 0;
   for (let v of stringifiedArray) {
-    if (v === '0' || v === '.') {
+    if (v === '0') {
       trailingZeros++;
     } else {
+      if (v === '.') trailingZeros++;
       break;
     }
   }
   const finalStringArray = stringifiedArray.slice(trailingZeros).reverse();
+  // handles > 0 but < 1, adds leading zero
   if (finalStringArray[0] === '.') finalStringArray.splice(0, 0, '0');
 
   // add symbols to final string
