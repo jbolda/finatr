a5419de1afe59071bd1e1a5f3944b2d20ec31792
Jacob Bolda <me@jacobbolda.com>
2025-10-25 02:20:16 -0500

    component test import

--- src/store/utils/dineroUtils.test.ts
+++ src/store/utils/dineroUtils.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/experimental-ct-react17';
+import { test, expect } from '@playwright/experimental-ct-react';
 
 import { toHumanInterest } from './dineroUtils';
 
