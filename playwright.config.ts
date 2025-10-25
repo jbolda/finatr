3bf559a083ad5e470512896b9b3f3570f09259dd
Jacob Bolda <me@jacobbolda.com>
2025-10-25 02:46:37 -0500

    fix url in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://127.0.0.1:1234`,
+    baseURL: `http://localhost:5173`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
@@ -83,7 +83,7 @@ export default defineConfig({
   ],
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:1234',
+    url: 'http://localhost:5173',
     reuseExistingServer: true
   }
 });
